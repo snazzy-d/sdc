b089148eca240b3323285d07c9c2eff1321587dc
Bernard Helyer <b.helyer@gmail.com>
2010-06-04 12:53:16 +1200

    treetojson.d: add header comment.

--- src/sdc/treetojson.d
+++ src/sdc/treetojson.d
@@ -1,3 +1,10 @@
+/**
+ * Copyright 2010 Bernard Helyer
+ * This file is part of SDC. SDC is licensed under the GPL.
+ * See LICENCE or sdl.d for more details.
+ * 
+ * treetojson.d: translate a parse tree into a JSON object.
+ */ 
 module sdc.treetojson;
 
 import libdjson.json;
