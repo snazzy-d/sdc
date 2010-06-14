44d2122244d0dd05c099b958b0dc95e1cc8c89cc
Bernard Helyer <b.helyer@gmail.com>
2010-06-14 16:26:13 +1200

    asttojson: added 'all' module.

---
+++ src/sdc/asttojson/all.d
@@ -0,0 +1,8 @@
+/**
+ * Copyright 2010 Bernard Helyer
+ * This file is part of SDC. SDC is licensed under the GPL.
+ * See LICENCE or sdl.d for more details.
+ */
+module sdc.asttojson.all;
+
+public import sdc.asttojson.base;
--- src/sdc/asttojson/base.d
+++ src/sdc/asttojson/base.d
@@ -2,7 +2,7 @@
  * Copyright 2010 Bernard Helyer
  * This file is part of SDC. SDC is licensed under the GPL.
  * See LICENCE or sdl.d for more details.
- */ 
+ */
 module sdc.asttojson.base;
 
 import libdjson.json;
