2073a9cf20291ce531c7abe42aea24f85d8cb37e
Bernard Helyer <b.helyer@gmail.com>
2010-06-17 23:15:22 +1200

    Get rid of half baked semantic stuff.

--- src/sdc/asttosemantic/base.d
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2010 Bernard Helyer
- * This file is part of SDC. SDC is licensed under the GPL.
- * See LICENCE or sdc.d for more details.
- */
-module sdc.asttosemantic.base;
-
-import ast = sdc.ast.all;
-import semantic = sdc.semantic.all;
