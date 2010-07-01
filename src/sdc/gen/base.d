758a3575b7e6f48a532badeb49a4ab3a43afeb9f
Bernard Helyer <b.helyer@gmail.com>
2010-07-01 15:02:04 +1200

    gen.base: simple two-pass compilation for functions.

--- src/sdc/gen/base.d
+++ src/sdc/gen/base.d
@@ -26,11 +26,28 @@ void genModule(Module mod, File file)
 {
     auto semantic = new Semantic();
     asmgen.emitComment(file, extractQualifiedName(mod.moduleDeclaration.name));
+    
+    // Find all top level declarations in the module; don't generate code.
+    foreach (declarationDefinition; mod.declarationDefinitions) {
+        declareDeclarationDefinition(declarationDefinition, file, semantic);
+    }
+    
+    // Actually generate the code for the declarations.
     foreach (declarationDefinition; mod.declarationDefinitions) {
         genDeclarationDefinition(declarationDefinition, file, semantic);
     }
 }
 
+
+void declareDeclarationDefinition(DeclarationDefinition declDef, File file, Semantic semantic)
+{
+    switch (declDef.type) {
+    case DeclarationDefinitionType.Declaration:
+        return declareDeclaration(cast(Declaration) declDef.node, file, semantic);
+    default:
+        break;
+    }
+}
 
 void genDeclarationDefinition(DeclarationDefinition declDef, File file, Semantic semantic)
 {
@@ -62,12 +79,39 @@ void genDeclarationBlock(DeclarationBlock declarationBlock, File file, Semantic 
     }
 }
 
+void declareDeclaration(Declaration declaration, File file, Semantic semantic)
+{
+    if (declaration.type == DeclarationType.Function) {
+        declareFunctionDeclaration(cast(FunctionDeclaration) declaration.node, file, semantic);
+    }
+}
+
 void genDeclaration(Declaration declaration, File file, Semantic semantic)
 {
     if (declaration.type == DeclarationType.Function) {
         genFunctionDeclaration(cast(FunctionDeclaration) declaration.node, file, semantic);
     } else if (declaration.type == DeclarationType.Variable) {
         genVariableDeclaration(cast(VariableDeclaration) declaration.node, file, semantic);
+    }
+}
+
+void declareVariableDeclaration(VariableDeclaration declaration, File file, Semantic semantic)
+{
+    bool global = semantic.currentScope is semantic.globalScope;
+    
+    auto primitive = fullTypeToPrimitive(declaration.type);
+    foreach (declarator; declaration.declarators) {
+        auto name = extractIdentifier(declarator.name);
+        auto syn  = new SyntheticVariableDeclaration();
+        syn.location = declaration.location;
+        syn.type = declaration.type;
+        syn.identifier = declarator.name;
+        syn.initialiser = declarator.initialiser;
+        try {
+            semantic.addDeclaration(name, syn, global);
+        } catch (RedeclarationError) {
+            error(declarator.location, format("'%s' is already defined", name));
+        }
     }
 }
 
@@ -121,12 +165,23 @@ void genDefaultInitialiser(File file, Semantic semantic, Variable var)
     return asmgen.emitStore(file, var, new Constant("0", removePointer(var.primitive)));
 }
 
+void declareFunctionDeclaration(FunctionDeclaration declaration, File file, Semantic semantic)
+{
+    string functionName = extractIdentifier(declaration.name);
+    try {
+        semantic.addDeclaration(functionName, declaration);
+    } catch (RedeclarationError) {
+        error(declaration.location, format("function '%s' is already defined", functionName));
+    }
+}
+
 void genFunctionDeclaration(FunctionDeclaration declaration, File file, Semantic semantic)
 {
+    bool global = semantic.currentScope is semantic.globalScope;
     asmgen.emitFunctionName(file, declaration);
     
     string functionName = extractIdentifier(declaration.name);
-    semantic.addDeclaration(functionName, declaration);
+    assert(semantic.findDeclaration(functionName, global));
     semantic.pushScope();
     foreach (i, parameter; declaration.parameters) if (parameter.identifier !is null) {
         auto var = new SyntheticVariableDeclaration();
