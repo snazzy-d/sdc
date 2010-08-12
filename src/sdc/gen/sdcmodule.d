132e07da6d4ae06a8e9403098865a7e1a72d8844
Bernard Helyer <b.helyer@gmail.com>
2010-08-12 17:36:42 +1200

    Fix reverse search.

--- src/sdc/gen/sdcmodule.d
+++ src/sdc/gen/sdcmodule.d
@@ -57,7 +57,7 @@ class Module
          * because of a bug manifested in std.range.retro.
          * WORKAROUND 2.048
          */
-        foreach (i; mScopeStack.length - 1 .. 0) {
+        for (auto i = mScopeStack.length - 1; i >= 0; i--) {
             auto localScope = mScopeStack[i];
             auto v = localScope.get(name);
             if (v !is null) {
