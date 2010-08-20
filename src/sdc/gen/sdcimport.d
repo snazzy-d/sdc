15734eb1c9a9852ea5412a4dec337b4b66eb8999
Bernard Helyer <b.helyer@gmail.com>
2010-08-20 19:58:42 +1200

    Add sdcimport to gen.

---
+++ src/sdc/gen/sdcimport.d
@@ -0,0 +1,14 @@
+/**
+ * Copyright 2010 Bernard Helyer
+ * This file is part of SDC. SDC is licensed under the GPL.
+ * See LICENCE or sdc.d for more details.
+ */
+module sdc.gen.sdcimport;
+
+import ast = sdc.ast.all;
+import sdc.gen.sdcmodule;
+
+
+void genImportDeclaration(ast.ImportDeclaration importDeclaration, Module mod)
+{
+}
