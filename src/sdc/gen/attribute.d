7ee9934356102c21b43d7aa25783ed2d2da9c543
Bernard Helyer <b.helyer@gmail.com>
2010-08-21 16:21:36 +1200

    Implement the canGen stuff for attributes.

--- src/sdc/gen/attribute.d
+++ src/sdc/gen/attribute.d
@@ -13,6 +13,31 @@ import ast = sdc.ast.all;
 import sdc.gen.base;
 import sdc.gen.sdcmodule;
 
+
+bool canGenAttributeSpecifier(ast.AttributeSpecifier attributeSpecifier, Module mod)
+{
+    return canGenAttribute(attributeSpecifier.attribute, mod);
+}
+
+bool canGenAttribute(ast.Attribute attribute, Module mod)
+{
+    final switch (attribute.type) with (ast.AttributeType) {
+    case Deprecated: case Private: case Package:
+    case Protected: case Public: case Export:
+    case Static: case Final: case Override:
+    case Abstract: case Const: case Auto:
+    case Scope: case __Gshared: case Shared:
+    case Immutable: case Inout: case atDisable:
+    case Align: case Extern: case ExternC:
+    case ExternCPlusPlus: case ExternD: case ExternWindows:
+    case ExternPascal: case ExternSystem:
+        return true;
+    case Pragma:
+        return false;
+    }
+    assert(false);
+}
+
 void declareAttributeSpecifier(ast.AttributeSpecifier attributeSpecifier, Module mod)
 {
     panic("OH SHIT THINGS ARE GETTING REFACTORED, YO!");
@@ -43,12 +68,12 @@ void genAttributeSpecifier(ast.AttributeSpecifier attributeSpecifier, Module mod
 void genAttribute(ast.Attribute attribute, Module mod)
 {
     with (ast.AttributeType) {
-    switch (attribute.type) {
-    case ExternC, ExternCPlusPlus, ExternD, ExternPascal, ExternWindows, ExternSystem:
-        mod.currentLinkage = cast(ast.Linkage) attribute.type;
-        break;
-    default:
-        panic(attribute.location, format("unhandled attribute type '%s'.", to!string(attribute.type)));
-    }
+        switch (attribute.type) {
+        case ExternC, ExternCPlusPlus, ExternD, ExternPascal, ExternWindows, ExternSystem:
+            mod.currentLinkage = cast(ast.Linkage) attribute.type;
+            break;
+        default:
+            panic(attribute.location, format("unhandled attribute type '%s'.", to!string(attribute.type)));
+        }
     }
 }
