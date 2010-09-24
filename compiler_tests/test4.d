9c6658ab24ca7815b02f41dfb601e8c8f8d26865
Bernard Helyer <b.helyer@gmail.com>
2010-09-25 01:08:46 +1200

    Added boolean cast test.

---
+++ compiler_tests/test4.d
@@ -0,0 +1,16 @@
+//T compiles:yes
+//T retval:12
+// Tests the casting of booleans to ints. 
+
+int main()
+{
+    bool a = false, b = true;
+    if (cast(int) a != 0) {
+        return 1;
+    }
+    if (cast(int) b != 1) {
+        return 2;
+    }
+    a = true;
+    return a + b + 10;
+}
