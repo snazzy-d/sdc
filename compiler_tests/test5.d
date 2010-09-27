d0a18e20c8d5eae1cb42b79847b089b9a80f5a1a
Bernard Helyer <b.helyer@gmail.com>
2010-09-27 21:01:31 +1300

    Made test5 a little harder. :)

--- compiler_tests/test5.d
+++ compiler_tests/test5.d
@@ -1,13 +1,13 @@
 //T compiles:yes
 //T retval:42
-// Tests simple functions.
-
-int add(int a, int b)
-{
-    return a + b;
-}
+// Tests simple functions, and use before definition.
 
 int main()
 {
     return add(21, add(20, 1));
+}
+
+int add(int a, int b)
+{
+    return a + b;
 }
