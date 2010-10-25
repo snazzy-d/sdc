2c4789df25890ed8854562617b7f0aa2dba5eb43
Jakob Ovrum <jakobovrum@gmail.com>
2010-10-25 07:20:38 +0200

    added test9, which tests strings and character literals, and string/pointer casts.

---
+++ compiler_tests/test9.d
@@ -0,0 +1,22 @@
+//T compiles:yes
+//T retval:0
+// Tests strings and character literals, and string/pointer casts.
+
+extern(C) uint strlen(const char* s);
+
+int main()
+{
+    char[] str = "test";
+    if (str.length != 4) {
+        return 1;
+    }
+    if (str[2] != 's') {
+        return 2;
+    }
+    char* p = str;
+    if(strlen(p) != str.length) {
+        return 3;
+    }
+    return 0;
+}
+
