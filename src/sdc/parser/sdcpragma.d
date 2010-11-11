67e663323dc81198d6c624d7fe3e55199a30db79
Bernard Helyer <b.helyer@gmail.com>
2010-11-11 19:57:40 +1300

    Added pragma parser.

---
+++ src/sdc/parser/sdcpragma.d
@@ -0,0 +1,21 @@
+module sdc.parser.sdcpragma;
+
+import sdc.tokenstream;
+import sdc.ast.sdcpragma;
+import sdc.parser.base;
+import sdc.parser.expression;
+
+
+Pragma parsePragma(TokenStream tstream)
+{
+    auto thePragma = new Pragma();
+    thePragma.location = tstream.peek.location;
+    
+    match(tstream, TokenType.Pragma);
+    match(tstream, TokenType.OpenParen);
+    thePragma.identifier = parseIdentifier(tstream);
+    if (tstream.peek.type == TokenType.Comma) {
+        thePragma.argumentList = parseArgumentList(tstream, TokenType.Comma, TokenType.CloseParen);
+    }
+    return thePragma;
+}
