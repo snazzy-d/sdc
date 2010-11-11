b75852fdcd9195df40135dcae2e84270e4cf8713
Bernard Helyer <b.helyer@gmail.com>
2010-11-11 20:02:16 +1300

    Comments.

--- src/sdc/ast/sdcpragma.d
+++ src/sdc/ast/sdcpragma.d
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2010 Bernard Helyer.
+ * This file is part of SDC. SDC is licensed under the GPL.
+ * See LICENCE or sdc.d for more details.
+ */
 module sdc.ast.sdcpragma;
 
 import sdc.ast.base;
--- src/sdc/parser/sdcpragma.d
+++ src/sdc/parser/sdcpragma.d
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2010 Bernard Helyer.
+ * This file is part of SDC. SDC is licensed under the GPL.
+ * See LICENCE or sdc.d for more details.
+ */
 module sdc.parser.sdcpragma;
 
 import sdc.tokenstream;
