1d7876a702c05b68809ba339ad37d52035397415
Jakob Bornecrantz <wallbraker@gmail.com>
2011-08-28 15:28:39 +0200

    Set mChar to dchar.init at eof
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/sdc/source.d
+++ src/sdc/source.d
@@ -127,7 +127,8 @@ class Source
     {
         if (mIndex >= source.length) {
             eof = true;
-            return dchar.init;
+            mChar = dchar.init;
+            return mChar;
         }
         
         if (mChar == '\n') {
