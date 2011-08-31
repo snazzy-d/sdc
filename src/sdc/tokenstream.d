7ee6cf50dfcb22d31b4d5ea31a83091f3cae1c01
Jakob Bornecrantz <wallbraker@gmail.com>
2011-08-31 18:28:41 +0200

    Comment TokenStream
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

--- src/sdc/tokenstream.d
+++ src/sdc/tokenstream.d
@@ -13,13 +13,29 @@ import sdc.compilererror;
 public import sdc.token;
 
 
+/**
+ * A stream of tokens, produced by @TokenWriter.
+ */
 class TokenStream
 {
+    /// The filename that this source was taken from.
     string filename;
     
+    /// All the tokens in this stream.
     private Token[] mTokens;
+    /// Pointing to the current token as returned by @peek and @getToken.
     private size_t mIndex;
 
+
+    /**
+     * Construct a new token stream with a given filename and token array.
+     *
+     * The internal pointer is set to the start of the stream. Also validates
+     * the stream is long enough and is started and terminated correctly.
+     *
+     * Throws:
+     *   CompilerPanic if array is malformed.
+     */
     this(string filename, Token[] tokens)
     {
         if (tokens.length < 3)
@@ -33,6 +49,12 @@ class TokenStream
         this.mTokens = tokens;
     }
     
+    /**
+     * Return the current token and advance the internal counter.
+     *
+     * Side-effects:
+     *   Internal pointer is incremented to the next token.
+     */
     Token get()
     {
         auto retval = mTokens[mIndex];
@@ -42,11 +64,20 @@ class TokenStream
         return retval;
     }
     
+    /**
+     * Return the current token.
+     */
     Token peek() @property
     {
         return mTokens[mIndex];
     }
     
+    /**
+     * Return the previous token.
+     *
+     * Throws:
+     *   CompilerPanic if accessing before the first token.
+     */
     Token previous() @property
     {
         if (mIndex < 1)
@@ -54,6 +85,13 @@ class TokenStream
         return mTokens[mIndex - 1];
     }
     
+    /**
+     * Return the token at @n steps before the current token.
+     * If @n is 0 the function is the same as @peek.
+     *
+     * Throws:
+     *   CompilerPanic if accessing before the first token.
+     */
     Token lookahead(size_t n)
     {
         if (n == 0) {
@@ -67,6 +105,13 @@ class TokenStream
         return mTokens[index];
     }
     
+    /**
+     * Return the token at @n steps behind the current token.
+     * If @n is 0 the function is the same as @peek.
+     *
+     * Throws:
+     *   CompilerPanic if accessing before the first token.
+     */
     Token lookbehind(size_t n)
     {
         auto index = mIndex - n;
