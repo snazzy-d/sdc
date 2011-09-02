1c274353de2a6696b6c689c162dddefe2fa6ef30
Jakob Bornecrantz <wallbraker@gmail.com>
2011-09-02 20:30:56 +0200

    Add test 60 for weird expression bug
    For issue #25.
    
    Signed-off-by: Jakob Bornecrantz <wallbraker@gmail.com>

---
+++ compiler_tests/test60.d
@@ -0,0 +1,12 @@
+//T compiles:no
+//T has-passed:no
+// Issue #25.
+
+int func1() { return 2; }
+int func2() { return 20; }
+int func3() { return 20; }
+
+int main()
+{
+    return func1() + func2() func3();
+}
