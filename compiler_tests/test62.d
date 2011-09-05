86f49e36bf98d7e886f317be0d66d7ad043232df
Jakob Ovrum <jakobovrum@gmail.com>
2011-09-05 17:14:29 +0900

    Added test62 testing slice expression

---
+++ compiler_tests/test62.d
@@ -0,0 +1,20 @@
+//T compiles:yes
+//T retval:0
+
+int main()
+{
+	string msg = "hello, world!";
+	char* cmsg = msg.ptr;
+	
+	string hello = msg[0..5];
+	assert(hello.length == 5);
+	assert(hello[0] == 'h');
+	assert(hello[4] == 'o');
+	
+	string world = cmsg[7..13];
+	assert(world.length == 6);
+	assert(world[0] == 'w');
+	assert(world[5] == '!');
+	
+	return 0;
+}
