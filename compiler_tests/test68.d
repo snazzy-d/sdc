a289efeccbadac8e5473c3fd0f1cbd74f662220c
Dejan Lekic <dejan@lekic.org>
2011-09-08 15:26:08 +0100

    Added lexer test for various comments.

---
+++ compiler_tests/test68.d
@@ -0,0 +1,60 @@
+//T compiles:yes
+//T retval:6
+//T desc:Various cases for comments.
+
+int main()
+{
+//      return 21;
+
+// //   return 31;
+
+        string a = /+ "+/" +/ 1";
+        string c = /* "*/" */ 1";
+        int    d = 1 + /* 2 */ + /+ 3 +/ // 4
+                   + 5;
+/*
+        return 22;
+*/
+
+/*      return 23; */
+
+/* /*   return 32; */
+
+/+
+        return 24;
++/
+
+/+      return 25; +/
+
+/+ /+   return 33; +/ +/
+
+// Testing nested comments
+
+// /*   return 26; */
+// /+   return 27; */
+
+/* //   return 28; */
+/* //   return 28; */
+
+/*
+     // return 29;
+*/
+
+/*
+     /* return 34;
+*/
+
+/*
+     /+ return 35;
+*/
+
+/+
+     // return 30;   
++/
+
+/+
+     /* return 36;
++/
+
+        return d;
+}
