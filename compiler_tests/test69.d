c9a63efddf694ecd8dcd9b7dfebf2bd77ae59b2f
Jakob Ovrum <jakobovrum@gmail.com>
2011-09-10 00:45:34 +0900

    Added test 69 testing illegal function pointer implicit conversions

---
+++ compiler_tests/test69.d
@@ -0,0 +1,13 @@
+//T compiles:no
+//? desc:Disallow implicit conversion between function pointers of different calling convention.
+
+extern(C) void foo(int a)
+{
+}
+
+int main()
+{
+	typeof(&foo) ok = &foo;
+	void function(int) bad = &foo;
+	return 0;
+}
