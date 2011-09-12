5f1a87c646987d67714f319869b4eed025e1dbad
Bernard Helyer <b.helyer@gmail.com>
2011-09-12 21:45:04 +1200

    Add some documentation to Value.

--- src/sdc/gen/value.d
+++ src/sdc/gen/value.d
@@ -31,24 +31,42 @@ import sdc.gen.type;
 import ast = sdc.ast.all;
 
 
+/**
+ * Value represents an instance of a given type.
+ * Every expression, even if temporary, will generate a Value.
+ */
 abstract class Value
 {
     /// The location that this Value was created at.
     Location location;
+    /// The access level that this Value inherited.
     ast.Access access;
+    /// If Value.lvalue is false, it can't be modified using set. 
     bool lvalue = false;
     
     ast.QualifiedName humanName;  // Optional.
     string mangledName;  // Optional.
     
+    /// The Module this Value is associated with.
     protected Module mModule;
+    /// The type this Value is an instance of.
     protected Type mType;
+    /**
+     * The underlying LLVM value for this Value.
+     * All types are stored as pointers to their types,
+     * so int's are stored as int*. To get the actual
+     * value use the get method.
+     */
     package LLVMValueRef mValue;
+    /// If true, this value was created at a global scope.
     protected bool mGlobal;
+    /// Callbacks that are called before a set takes place.
     protected void delegate(Value val)[] mSetPreCallbacks;
-    protected void delegate(Value val)[] mSetPostCallbacks;
+    /// Callbacks that are called after a set takes place.
+    protected void delegate(Value va)[] mSetPostCallbacks;
     protected bool mIsKnown = false;
     
+    /// All Values should be able to be created with just a Module and a Location.
     this(Module mod, Location loc)
     {
         mModule = mod;
@@ -57,9 +75,11 @@ abstract class Value
         mGlobal = mod.currentScope is mod.globalScope;
     }
     
+    /// If true, the value of this Value is known to the compiler.
     @property bool isKnown() { return mIsKnown; }
     @property void isKnown(bool b) { mIsKnown = b; }
     
+    /// If isKnown, these are the types the compiler knows about, and contain the actual value.
     union
     {
         bool knownBool;
@@ -90,6 +110,10 @@ abstract class Value
         mType = t;
     }
     
+    /**
+     * Generate code to cast this Value to the given type.
+     * This method isn't concerned with narrowing or the like.
+     */
     Value performCast(Location location, Type t)
     {
         throw new CompilerPanic(location, "invalid cast");
@@ -106,10 +130,13 @@ abstract class Value
     {
         throw new CompilerPanic(format(`attempt to %s value of type "%s" failed.`, s, type.name()));
     }
-        
+
+    /// Retrieve the actual LLVM value that matches the Type. 
     LLVMValueRef get() { fail("get"); assert(false); }
+    /// Set Value's value. Is affected by lvalue.
     void set(Location location, Value val) { fail("set (by Value)"); assert(false); }
     void set(Location location, LLVMValueRef val) { fail("set (by LLVMValueRef)"); assert(false); }
+    /// The same as set, but not affected by lvalue.
     void initialise(Location location, Value val) 
     {
         const bool islvalue = lvalue;
@@ -124,30 +151,37 @@ abstract class Value
         set(location, val);
         lvalue = islvalue; 
     }
-    Value add(Location loc, Value val) { fail(loc, "add"); assert(false); }
-    Value inc(Location loc) { fail(loc, "increment"); assert(false); }
-    Value dec(Location loc) { fail(loc, "decrement"); assert(false); }
-    Value sub(Location loc, Value val) { fail(loc, "subtract"); assert(false); }
-    Value mul(Location loc, Value val) { fail(loc, "multiply"); assert(false); }
-    Value div(Location loc, Value val) { fail(loc, "divide"); assert(false); }
-    Value eq(Location loc, Value val) { fail(loc, "compare equality of"); assert(false); }
-    Value identity(Location loc, Value val) { return eq(loc, val); }
-    Value nidentity(Location loc, Value val) { return neq(loc, val); }
-    Value neq(Location loc, Value val) { fail(loc, "compare non-equality of"); assert(false); }
-    Value gt(Location loc, Value val) { fail(loc, "compare greater-than of"); assert(false); }
-    Value lt(Location loc, Value val) { fail(loc, "compare less-than of"); assert(false); }
-    Value lte(Location loc, Value val) { fail(loc, "compare less-than of"); assert(false); }
-    Value or(Location loc, Value val) { fail(loc, "or"); assert(false); }
-    Value and(Location loc, Value val) { fail(loc, "and"); assert(false); }
-    Value xor(Location loc, Value val) { fail(loc, "xor"); assert(false); }
-    Value not(Location loc) { fail(loc, "not"); assert(false); }
-    Value dereference(Location loc) { fail(loc, "dereference"); assert(false); }
-    Value index(Location loc, Value val) { fail(loc, "index"); assert(false); }
-    Value slice(Location loc, Value from, Value to) { fail(loc, "slice"); assert(false); }
-    Value mod(Location loc, Value val) { fail(loc, "modulo"); assert(false); }
-    Value getSizeof(Location loc) { fail(loc, "getSizeof"); assert(false); }
-        
-        
+    
+    /** 
+     * Most of the operators don't modify this instance,
+     * instead creating a new one. Even inc and dec.
+     * Obvious exceptions are set and initialise.
+     */
+    
+    Value add(Location loc, Value val) { fail(loc, "add"); assert(false); }  /// +
+    Value inc(Location loc) { fail(loc, "increment"); assert(false); }  /// v + 1
+    Value dec(Location loc) { fail(loc, "decrement"); assert(false); }  /// v - 1
+    Value sub(Location loc, Value val) { fail(loc, "subtract"); assert(false); }  /// -
+    Value mul(Location loc, Value val) { fail(loc, "multiply"); assert(false); }  /// *
+    Value div(Location loc, Value val) { fail(loc, "divide"); assert(false); }  /// /
+    Value eq(Location loc, Value val) { fail(loc, "compare equality of"); assert(false); }   /// ==
+    Value identity(Location loc, Value val) { return eq(loc, val); }  /// is
+    Value nidentity(Location loc, Value val) { return neq(loc, val); }  /// !is
+    Value neq(Location loc, Value val) { fail(loc, "compare non-equality of"); assert(false); }  /// !=
+    Value gt(Location loc, Value val) { fail(loc, "compare greater-than of"); assert(false); }  /// >
+    Value lt(Location loc, Value val) { fail(loc, "compare less-than of"); assert(false); }  /// <
+    Value lte(Location loc, Value val) { fail(loc, "compare less-than of"); assert(false); }  /// <=
+    Value or(Location loc, Value val) { fail(loc, "or"); assert(false); }  /// |
+    Value and(Location loc, Value val) { fail(loc, "and"); assert(false); }  /// &
+    Value xor(Location loc, Value val) { fail(loc, "xor"); assert(false); }  /// ^
+    Value not(Location loc) { fail(loc, "not"); assert(false); }  /// ~
+    Value dereference(Location loc) { fail(loc, "dereference"); assert(false); }  /// *v
+    Value index(Location loc, Value val) { fail(loc, "index"); assert(false); }  /// [n]
+    Value slice(Location loc, Value from, Value to) { fail(loc, "slice"); assert(false); }  /// [n .. m]  
+    Value mod(Location loc, Value val) { fail(loc, "modulo"); assert(false); }  /// %
+    Value getSizeof(Location loc) { fail(loc, "getSizeof"); assert(false); }  /// .sizeof
+    
+    /// ||
     Value logicalOr(Location location, Value val)
     {
         auto boolType = new BoolType(mModule);
@@ -156,6 +190,7 @@ abstract class Value
         return a.or(location, b);
     }
     
+    /// &&
     Value logicalAnd(Location location, Value val)
     {
         auto boolType = new BoolType(mModule);
@@ -164,6 +199,7 @@ abstract class Value
         return a.and(location, b);
     }
     
+    /// !
     Value logicalNot(Location location)
     {
         auto boolType = new BoolType(mModule);
@@ -171,7 +207,7 @@ abstract class Value
         return a.not(location);
     }
     
-    
+    /// &v
     Value addressOf(Location location)
     {
         //errorIfNotLValue(location, "cannot take the address of an rvalue.");
