46b5b3b5cc9b8e2a5b6901090b668ed79be43971
Bernard Helyer <b.helyer@gmail.com>
2011-09-12 23:47:34 +1200

    Restore Jakob's honour.

--- compiler_tests/test73.d
+++ compiler_tests/test73.d
@@ -1,33 +1,9 @@
-//? desc:Test the while loop.
+//T compiles:yes
+//T retval:0
 
 int main()
 {
-	int i = 0;
-	while(i < 10)
-		i++;
-	
-	assert(i == 10);
-	
-	// Break.
-	i = 0;
-	while(i < 10)
-	{
-		i++;
-		if(i == 5)
-			break;
-	}
-	assert(i == 5);
-	
-	// Continue.
-	i = 0;
-	int j = 0;
-	while(i < 10)
-	{
-		i++;
-		if(i > 5)
-			continue;
-		j++;
-	}
-	assert(j == 5);
-	return 0;
+    return 0;
 }
+/*/**/
+
