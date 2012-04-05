cf7ab469e1349fcdc6907bf945842add7ef9d6ab
deadalnix <deadalnix@gmail.com>
2012-04-05 14:38:20 +0200

    Some more playing with types.

--- src/sdc/ast/type2.d
+++ src/sdc/ast/type2.d
@@ -5,23 +5,67 @@ import sdc.ast.base;
 
 class Type : Node {
 	this(Location location) {
-		
+		this.location = location;
 	}
 	
-	abstract Type makeConst() const;
-	abstract Type makeImmutable() const;
-	abstract Type makeMutable() const;
+	abstract Type makeMutable();
+	
+	abstract Type makeImmutable();
+	
+	abstract Type makeConst();
+	
+	abstract Type makeInout();
+}
+
+class SimpleStorageClassType : Type {
+	private uint storageClass = 0;
+	
+	enum MUTABLE = 0x00;
+	enum IMMUTABLE = 0x01;
+	enum CONST = 0x02;
+	enum INOUT = 0x03;
+	
+	enum MASK = ~0x03;
+	
+	this(Location location) {
+		super(location);
+	}
+	
+final:	// Check whenever these operation make sense.
+	override Type makeMutable() {
+		storageClass &= MASK;
+		return this;
+	}
+	
+	override Type makeImmutable() {
+		makeMutable();
+		storageClass |= IMMUTABLE;
+		return this;
+	}
+	
+	override Type makeConst() {
+		makeMutable();
+		storageClass |= CONST;
+		return this;
+	}
+	
+	override Type makeInout() {
+		makeMutable();
+		storageClass |= INOUT;
+		return this;
+	}
 }
 
 template isBuiltin(T) {
 	import std.traits;
 	
-	enum bool isBuiltin = isNumeric!T || isSomeChar!T || is(T : bool) || is(Unqual!T == void);
+	enum bool isBuiltin = isNumeric!T || isSomeChar!T || is(Unqual!T == bool) || is(Unqual!T == void);
 }
 
+/**
+ * All basics types and qualified basic types.
+ */
 template basicType(T) if(isBuiltin!T) {
-	pragma(msg, T.stringof);
-	
 	class BasicType : Type {
 		private this() {
 			super(Location.init);
@@ -39,6 +83,10 @@ template basicType(T) if(isBuiltin!T) {
 			import std.traits;
 			return .basicType!(Unqual!T);
 		}
+		
+		override Type makeInout() const {
+			return .basicType!(inout T);
+		}
 	}
 	
 	immutable BasicType basicType;
@@ -48,11 +96,14 @@ template basicType(T) if(isBuiltin!T) {
 	}
 }
 
-unittest {
-	void foo(T)(T t) {
-	}
+class IdentifierType : SimpleStorageClassType {
+	// TODO: use identifier as objects to support template intances.
+	private string[] identifiers;
 	
-	foo(basicType!int);
-	foo(basicType!void);
+	this(Location location, string[] identifiers) {
+		super(location);
+		
+		this.identifiers = identifiers;
+	}
 }
 
--- src/sdc/parser/type2.d
+++ src/sdc/parser/type2.d
@@ -19,17 +19,93 @@ auto parseBasicType(TokenStream tstream) {
 			match(tstream, TokenType.CloseParen);
 			
 			return qualifyType(type);
-		} else {
-			return qualifyType(parseType(tstream));
 		}
+		
+		return qualifyType(parseType(tstream));
 	}
 	
 	switch(tstream.peek.type) {
 		case TokenType.Const :
-			return processQualifier!(function Type(type) { return type.makeConst(); })();
+			return processQualifier!(function(Type type) { return type.makeConst(); })();
+		case TokenType.Immutable :
+			return processQualifier!(function(Type type) { return type.makeImmutable(); })();
+		case TokenType.Mutable :
+			return processQualifier!(function(Type type) { return type.makeMutable(); })();
+		case TokenType.Inout :
+			return processQualifier!(function(Type type) { return type.makeInout(); })();
+		case TokenType.Identifier :
+			return parseIdentifierType(tstream, tstream.get().value);
+		case TokenType.Dot :
+			tstream.get();
+			return parseIdentifierType(tstream, "");
+		case TokenType.Typeof :
+			// TODO: Handle typeof.
+			assert(0);
+		case TokenType.Bool :
+			return basicType!bool;
+		case TokenType.Byte :
+			return basicType!byte;
+		case TokenType.Ubyte :
+			return basicType!ubyte;
+		case TokenType.Short :
+			return basicType!short;
+		case TokenType.Ushort :
+			return basicType!ushort;
+		case TokenType.Int :
+			return basicType!int;
+		case TokenType.Uint :
+			return basicType!uint;
+		case TokenType.Long :
+			return basicType!long;
+		case TokenType.Ulong :
+			return basicType!ulong;
+/*		case TokenType.Cent :
+			return basicType!cent;
+		case TokenType.Ucent :
+			return basicType!ucent;	*/
+		case TokenType.Char :
+			return basicType!char;
+		case TokenType.Wchar :
+			return basicType!wchar;
+		case TokenType.Dchar :
+			return basicType!dchar;
+		case TokenType.Float :
+			return basicType!float;
+		case TokenType.Double :
+			return basicType!double;
+		case TokenType.Real :
+			return basicType!real;
+/*		case TokenType.Ifloat :
+			return basicType!ifloat;
+		case TokenType.Idouble :
+			return basicType!idouble;
+		case TokenType.Ireal :
+			return basicType!ireal;
+		case TokenType.Cfloat :
+			return basicType!cfloat;
+		case TokenType.Cdouble :
+			return basicType!cdouble;
+		case TokenType.Creal :
+			return basicType!creal;	*/
+		case TokenType.Void :
+			return basicType!void;
 		default :
 			// TODO: handle.
 			assert(0);
 	}
+}
+
+auto parseIdentifierType(TokenStream tstream, string name) {
+	string[] identifiers = [name];
+	auto location = tstream.peek.location;
+	
+	while(tstream.peek.type == TokenType.Dot) {
+		tstream.get();
+		identifiers ~= match(tstream, TokenType.Identifier).value;
+	}
+	
+	location.spanTo(tstream.previous.location);
+	
+	return new IdentifierType(location, identifiers);
 }
 
