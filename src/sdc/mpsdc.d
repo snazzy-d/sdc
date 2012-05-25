0d6a31c37bd5e623e0447e2468454b21b9796a63
Bernard Helyer <b.helyer@gmail.com>
2012-05-26 04:23:36 +1200

    Lex files passed to compiler.

--- src/sdc/mpsdc.d
+++ src/sdc/mpsdc.d
@@ -4,10 +4,32 @@
 module sdc.mpsdc;
 version (SDCMULTIPASS):
 
-import std.stdio;
+import std.stdio : writeln, stderr, stdout;
+import std.file : exists;
+
+import sdc.compilererror : CompilerError;
+import sdc.lexer : lex;
+import sdc.source : Source;
+import sdc.tokenstream : TokenStream;
 
 
-void main(string[] args)
+int main(string[] args)
 {
-    writeln("Hello, multi-pass world!");
+    if (args.length == 1) {
+        stderr.writeln("usage: sdc file");
+        return 1;
+    }
+
+    foreach (file; args[1..$]) {
+        compile(file);
+    }
+
+    return 0;
+}
+
+void compile(string filename)
+{
+    auto src = new Source(filename);
+    TokenStream tstream = lex(src);
+    tstream.printTo(stdout);
 }
