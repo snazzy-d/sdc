e57d1f095bbab5f742f4a91de15f4ba55e3fe3a2
Bernard Helyer <b.helyer@gmail.com>
2012-05-26 04:11:05 +1200

    Added multipass scaffolding
    Trying an experiment. Step one!

---
+++ src/sdc/mpsdc.d
@@ -0,0 +1,13 @@
+/**
+ * Entry point for the new multi-pass experiment.
+ */
+module sdc.mpsdc;
+version (SDCMULTIPASS):
+
+import std.stdio;
+
+
+void main(string[] args)
+{
+    writeln("Hello, multi-pass world!");
+}
