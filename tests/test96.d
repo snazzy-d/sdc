f9cd9e62ab13d80e0fbda922782da129d1187f7b
deadalnix <deadalnix@gmail.com>
2012-09-26 13:34:32 +0200

    new test case for eponymous trick and bool type parameter.

---
+++ tests/test96.d
@@ -0,0 +1,18 @@
+//T compiles:yes
+//T retval:12
+
+template Foo(T)
+{
+    T Foo;
+}
+
+int main()
+{
+    Foo!int = 4;
+    Foo!long = 2 * Foo!int;
+    
+    Foo!int = 4 + cast(typeof(Foo!int)) (Foo!long + Foo!bool);
+    
+    return Foo!int;
+}
+
