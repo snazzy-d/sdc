472c9e75cfb87a25089cd89090bf9d512fd4414c
deadalnix <deadalnix@gmail.com>
2013-03-11 02:51:40 +0900

    handle other encoding than utf-8

--- libd-llvm/libd/src/d/location.d
+++ libd-llvm/libd/src/d/location.d
@@ -1,5 +1,10 @@
 module d.location;
 
+import std.utf;
+import std.range;
+import std.string;
+import std.system;
+
 /**
  * Struct representing a location in a source file.
  */
@@ -45,7 +50,8 @@ final class FileSource : Source {
 		_filename = filename;
 		
 		import std.file;
-		super(cast(string) read(filename) ~ '\0');
+		auto data = cast(const(ubyte)[]) read(filename);
+		super(convertToUTF8(data) ~ '\0');
 	}
 	
 	override string format(const Location location) const {
@@ -75,5 +81,44 @@ final class MixinSource : Source {
 	override string filename() const {
 		return location.source.filename;
 	}
+}
+
+/// Given data, it looks at the BOM to detect which encoding, and converts
+/// the text from that encoding into UTF-8.
+string convertToUTF8(const(ubyte)[] data) {
+	if (data.startsWith([0xEF, 0xBB, 0xBF])) {
+		// UTF-8 (toUTF8 is for validation purposes)
+		return toUTF8(cast(string) data[3 .. $].idup);
+	} else if (data.startsWith([0x00, 0x00, 0xFE, 0xFF])) {
+		// UTF-32 BE
+		return convertToUTF8Impl!(dchar, Endian.bigEndian)(data);
+	} else if (data.startsWith([0xFF, 0xFE, 0x00, 0x00])) {
+		// UTF-32 LE
+		return convertToUTF8Impl!(dchar, Endian.littleEndian)(data);
+	} else if (data.startsWith([0xFE, 0xFF])) {
+		// UTF-16 BE
+		return convertToUTF8Impl!(wchar, Endian.bigEndian)(data);
+	} else if (data.startsWith([0xFF, 0xFE])) {
+		// UTF-16 LE
+		return convertToUTF8Impl!(wchar, Endian.littleEndian)(data);
+	} else {
+		// ASCII
+		return toUTF8(cast(string)data.idup);
+	}
+}
+
+string convertToUTF8Impl(CType, Endian end)(const(ubyte)[] data) {
+	enum cpsize = CType.sizeof;
+	data = data[cpsize .. $];
+	CType[] res;
+	foreach(i; iota(0, data.length, cpsize)) {
+		auto buf = data[i .. i+cpsize].dup;
+		static if (end != endian) {
+			buf = buf.reverse;
+		}
+		res ~= *(cast(CType*)buf.ptr);
+	}
+	
+	return toUTF8(res);
 }
 
