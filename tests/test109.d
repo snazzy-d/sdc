3618665af39cd1844d47b55b814ee70f45c86eb1
deadalnix <deadalnix@gmail.com>
2013-03-24 23:14:29 +0900

    Add a test that use identifier[] ambiguity and imbicated switch.

---
+++ tests/test109.d
@@ -0,0 +1,41 @@
+//T compiles:yes
+//T has-passed:yes
+//T retval:42
+
+int foo(char* p) {
+	if(*p == '\0') {
+		return 0;
+	}
+	
+	switch(*p) {
+		case 'i' :
+			p++;
+			if(*p == '\0') {
+				return 1;
+			}
+			
+			switch(*p) {
+				case 'f' :
+					return 2;
+				
+				case 's' :
+					return 3;
+				
+				default :
+					return 42;
+			}
+		
+		default :
+			return 1;
+	}
+}
+
+int main() {
+	char[3] str;
+	str[0] = 'i';
+	str[1] = 'g';
+	str[2] = '\0';
+	
+	return foo(&str[0]);
+}
+
