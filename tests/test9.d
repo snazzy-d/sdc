28fafcd07136282aaff1938765d52fa704986498
deadalnix <deadalnix@gmail.com>
2013-08-04 22:36:19 +0900

    Code formating in some tests

--- tests/test9.d
+++ tests/test9.d
@@ -1,22 +1,25 @@
 //T compiles:yes
 //T retval:0
+//T has-passed:yes
 // Tests strings and character literals, and string/pointer casts.
 
 extern(C) size_t strlen(const char* s);
 
-int main()
-{
-    string str = "test";  
-    if (str.length != 4) {
-        return 1;
-    }
-    if (str[2] != 's') {
-        return 2;
-    }
-    auto p = str.ptr;
-    if(strlen(p) != str.length) {
-        return 3;
-    }
+int main() {
+	string str = "test";  
+	if(str.length != 4) {
+		return 1;
+	}
+	
+	if(str[2] != 's') {
+		return 2;
+	}
+	
+	auto p = str.ptr;
+	if(strlen(p) != str.length) {
+		return 3;
+	}
+	
     return 0;
 }
 
