8e48daabbf37ee0ebd248a263b8175dd1c93dcb8
deadalnix <deadalnix@gmail.com>
2013-11-09 19:18:53 -0800

    Member function overload

---
+++ tests/test136.d
@@ -0,0 +1,26 @@
+//T compiles:yes
+//T has-passed:yes
+//T retval:42
+// Method overload.
+
+struct Foo {
+	int i;
+	
+	this(int i) {
+		this.i = i;
+	}
+	
+	auto bar(int j) {
+		return i + j;
+	}
+	
+	int bar() {
+		return bar(-5);
+	}
+}
+
+int main() {
+	auto f = Foo(47);
+	return f.bar();
+}
+
