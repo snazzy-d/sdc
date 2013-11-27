1496a4a86a11d87589ea969df13081f9ac6b5ca4
deadalnix <deadalnix@gmail.com>
2013-11-27 00:26:37 -0800

    scope(exit)

---
+++ tests/test140.d
@@ -0,0 +1,18 @@
+//T compiles:yes
+//T has-passed:yes
+//T retval:35
+// Scope exit.
+
+int a = 10;
+
+int foo() {
+	a++;
+	scope(exit) a = a * 2;
+	
+	return a++;
+}
+
+int main() {
+	return foo() + a;
+}
+
