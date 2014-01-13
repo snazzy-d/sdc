10caf8221b2d235d1865285e802b44d12e524df6
deadalnix <deadalnix@gmail.com>
2014-01-12 18:46:02 -0800

    Update runner for 2.065 because of hange in std.getopt behavior.

--- tests/runner.d
+++ tests/runner.d
@@ -103,7 +103,7 @@ void test(string filename, string compiler)
         file.remove(exeName);
     }
     if (compiler == SDC) {
-        command = format(`%s -o=%s "%s" %s`, SDC, exeName, filename, cmdDeps);
+        command = format(`%s -o %s "%s" %s`, SDC, exeName, filename, cmdDeps);
     } else {
         command = format(`%s %s "%s" %s`, compiler, exeName, filename, cmdDeps);
     }
