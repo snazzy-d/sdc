a3a43756932e7b644f5fb57b072e6518c2b2c8a8
deadalnix <deadalnix@gmail.com>
2014-06-07 20:05:23 -0700

    Closures

---
+++ tests/test0153.d
@@ -0,0 +1,18 @@
+//T compiles:yes
+//T has-passed:yes
+//T retval:36
+// Closure
+
+int main() {
+	int a = 10;
+	int foo() {
+		return --a;
+	}
+	
+	return () {
+		return a++;
+	}() + {
+		return a -= 2;
+	}() + foo() + ((int b) => a + b)(1);
+}
+
