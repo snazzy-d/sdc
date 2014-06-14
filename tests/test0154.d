2666e2329d28b8a40f897870e46524e3379f8697
deadalnix <deadalnix@gmail.com>
2014-06-14 16:44:42 -0700

    Template value parameters

---
+++ tests/test0154.d
@@ -0,0 +1,15 @@
+//T compiles:yes
+//T has-passed:yes
+//T retval:25
+// template value parameter
+
+auto foo(T U, T)() {
+	return U + T.sizeof;
+}
+
+int main() {
+	return foo!true() + foo!10() + foo!I();
+}
+
+enum I = 5;
+
