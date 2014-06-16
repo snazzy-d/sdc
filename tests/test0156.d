0009ad9203b853dbf3f88b33f58f4eedd6543059
deadalnix <deadalnix@gmail.com>
2014-06-15 19:10:53 -0700

    Chain and return closures

---
+++ tests/test0156.d
@@ -0,0 +1,21 @@
+//T compiles:yes
+//T has-passed:yes
+//T retval:36
+// Closure chaining
+
+int main() {
+	int a = 11;
+	
+	auto foo() {
+		int b = 25;
+		
+		auto bar() {
+			return a + b;
+		}
+		
+		return bar;
+	}
+	
+	return foo()();
+}
+
