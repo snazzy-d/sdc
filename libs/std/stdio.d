38e9f59044549501ec417c39aee7b5bb17bde365
Stefan K <uplink.coder@gmail.com>
2014-10-03 17:40:10 +0000

    Temporary fix to the SDC runtime to allow printing strings.

--- libs/std/stdio.d
+++ libs/std/stdio.d
@@ -1,9 +1,9 @@
 /// Part of a 'mock' phobos used for testing. Not intended for real use.
 module std.stdio;
+void writeln(string s) {
+	printf("%.*s\n".ptr, s.length, s.ptr);
+}
 
-
-void writeln(string s)
-{
-    printf(s);
-    printf("\n");
+void writeln(int i) {
+	printf("%d\n".ptr, i);
 }
