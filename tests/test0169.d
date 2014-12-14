110e4a55b9dcb55665bfd8129e1f0241f8400d08
Stefan Koch <uplink.coder@googlemail.com>
2014-12-14 19:48:09 +0000

    fi

--- tests/test0169.d
+++ tests/test0169.d
@@ -17,13 +17,18 @@ int main() {
 	assert(bl);
 	bl = 0;// false
 	assert(!bl);
+	bl = 1-1;
+	assert(!bl);
 	bl = true-1; //false
 	assert(!bl);
 	bl = (-1UL)-(-1UL-1); // true
 	assert(bl);
 
+
 	byte b = -128;
-	long l = -1UL; 
+	
+	long l = -1UL;
+	short s1 = -1UL-255+265+(-1UL); 
 	ubyte ub = 42;
 	ubyte ub2 = 255; 	
 	byte b2 = -42;
@@ -33,7 +38,8 @@ int main() {
 	int i = b+b+b+b+b+b+b-ush;
 	S s;
 	s.sub = 128;
-	ush = cast (int) ushort.max;
+	ushort ush2 = cast(int) cast(byte) ulong.max;
+	assert(ush2 == ushort.max);
 	byte b3 = ub+ushort.max-short.max+short.min-128;
 	return b3 + s.sub;
  
