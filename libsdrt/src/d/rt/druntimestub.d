5501d1df5da8d064265cedefe8b49f4afbbaef71
deadalnix <deadalnix@gmail.com>
2015-03-15 15:12:43 -0700

    Add message in druntime stubs

--- libsdrt/src/d/rt/druntimestub.d
+++ libsdrt/src/d/rt/druntimestub.d
@@ -6,15 +6,18 @@ void* _d_allocmemory(size_t size) {
 	return malloc(size);
 }
 
-void _d_assert(string, int) {
+void _d_assert(string file, int line) {
+	printf("assert fail: %s:%d\n".ptr, file.ptr, line);
 	exit(1);
 }
 
-void _d_assert_msg(string, string, int) {
+void _d_assert_msg(string msg, string file, int line) {
+	printf("%s: %s:%d\n".ptr, msg.ptr, file.ptr, line);
 	exit(1);
 }
 
-void _d_arraybounds(string, int) {
+void _d_arraybounds(string file, int line) {
+	printf("bound check fail: %s:%d\n".ptr, file.ptr, line);
 	exit(1);
 }
 
