e8fe8eec9270c3241464a4b894fd92ffb2166589
deadalnix <deadalnix@gmail.com>
2015-04-04 19:01:07 -0700

    Fix test for opAssign as per http://forum.dlang.org/thread/dqkqvnkgxkxncyvyitid@forum.dlang.org

--- tests/test0167.d
+++ tests/test0167.d
@@ -1,15 +1,15 @@
 //T compiles:yes
 //T has-passed:yes
-//T retval:53
+//T retval:57
 // Tests TernaryOperator
 
 int main() {
 	int ret = 5;
 	ret += ((ret++ == 5) ? (ret += 3) : (ret -= 11)) + ret;
-	assert(ret == 23);
+	assert(ret == 27);
 
 	ret += ((ret-- == 22) ? (ret += 5) : (ret -= 7)) + ret;
-	assert(ret == 53);
+	assert(ret == 57);
 
 	return ret;
 }
