03227a426af9992c3d9a426a1ccc4e15035658dd
deadalnix <deadalnix@gmail.com>
2016-02-13 17:28:47 -0800

    Use system's llvm-lit

--- libd-llvm/tests/runlit.py
+++ libd-llvm/tests/runlit.py
@@ -1,5 +1,15 @@
 #!/usr/bin/env python
-# wrapper to run lit from commandline
+
+import os
+llvmconfig = os.getenv('LLVM_CONFIG', 'llvm-config')
+
+import subprocess
+llvm_source_root = subprocess.check_output([llvmconfig, "--src-root"]).strip()
+
+# Make sure we can find the lit package.
+import sys
+sys.path.insert(0, os.path.join(llvm_source_root, 'utils', 'lit'))
+
 if __name__=='__main__':
     import lit
     lit.main()
