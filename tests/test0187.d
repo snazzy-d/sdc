bea788902d60af0731f671fa5eb97b2752bb3c0f
Vivek Pandya <vivekvpandya@gmail.com>
2016-02-15 20:30:00 +0530

    Patch to fix the issue 192 'no error on missing default in non-final switches' It changes libd/src/d/semantic/flow.d and adds following new test cases: tests/test0184.d tests/test0185.d tests/test0186.d tests/test0187.d

---
+++ tests/test0187.d
@@ -0,0 +1,15 @@
+//T compiles:no
+//T retval:0
+//T has-passed:yes
+// Tests missing default in non-final switches
+
+int main() {
+	int x = 10;
+	switch(x) {
+		case 0:
+			return 1;
+		case 2:
+			return 2;
+	}
+}
+
