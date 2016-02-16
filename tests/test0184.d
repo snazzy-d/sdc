d3ab7a2ac5c964a1e6541c9faee678d2d83b4906
Vivek Pandya <vivekvpandya@gmail.com>
2016-02-16 14:26:13 +0530

    This is patch for issue no 192 'no error on missing defaut in non-final switches' It check for LabeledStatements with "default" label and sets flag to indicate such statement. If flag is already set and such statement found it reports error. No Default statement found then also reports error. nested cases is also handled. Changes in file : libd/src/d/semantic/flow.d New files : tests/test0184.d             tests/test0185.d             tests/test0186.d             tests/test0187.d

---
+++ tests/test0184.d
@@ -0,0 +1,19 @@
+//T compiles:no
+//T retval:0
+//T has-passed:yes
+// Tests multiple default in non-final switches
+
+int main() {
+	int x = 10;
+	switch(x) {
+		case 0:
+			return 1;
+		case 2:
+			return 2;
+		default:
+			return 5;
+		default:
+			return 7;
+	}
+}
+
