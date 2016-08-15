62f25092cdef8cf2423045b08901157cc0d8b870
amaury <amaury@fb.com>
2016-08-15 14:46:41 +0200

    Fix intrinsic test

--- libd-llvm/tests/intrinsics.d
+++ libd-llvm/tests/intrinsics.d
@@ -4,14 +4,14 @@ import sdc.intrinsics;
 
 bool likely(bool b) {
 	return expect(b, true);
-// CHECL-LABEL: _D5tests9libd-llvm10intrinsics6likelyFMbZb
+// CHECK-LABEL: _D10intrinsics6likelyFMbZb
 // CHECK: [[RET:%[a-z0-9\.]+]] = call i1 @llvm.expect.i1(i1 {{.*}}, i1 true)
 // CHECK: ret i1 [[RET]]
 }
 
 bool unlikely(bool b) {
 	return expect(b, false);
-// CHECL-LABEL: _D5tests9libd-llvm10intrinsics8unlikelyFMbZb
+// CHECK-LABEL: _D10intrinsics8unlikelyFMbZb
 // CHECK: [[RET:%[a-z0-9\.]+]] = call i1 @llvm.expect.i1(i1 {{.*}}, i1 false)
 // CHECK: ret i1 [[RET]]
 }
@@ -19,6 +19,6 @@ bool unlikely(bool b) {
 bool docas(uint* ptr, uint old, uint val) {
 	auto cr = cas(ptr, old, val);
 	return cr.success;
-// CHECL-LABEL: _D5tests9libd-llvm10intrinsics5docasFMbZb
+// CHECK-LABEL: _D10intrinsics5docasFMPkkkZb
 // CHECK: [[RET:%[a-z0-9\.]+]] = cmpxchg i32* {{.*}}, i32 {{.*}}, i32 {{.*}} seq_cst seq_cst
 }
