4058d91e8802c856e194d8b7c46fbc8d2db06d36
Amaury SECHET <deadalnix@gmail.com>
2017-01-13 15:19:31 +0100

    Add lit tests for popcount, leading and trailing zero count

--- libd-llvm/tests/intrinsics.d
+++ libd-llvm/tests/intrinsics.d
@@ -22,3 +22,34 @@ bool docas(uint* ptr, uint old, uint val) {
 // CHECK-LABEL: _D10intrinsics5docasFMPkkkZb
 // CHECK: [[RET:%[a-z0-9\.]+]] = cmpxchg i32* {{.*}}, i32 {{.*}}, i32 {{.*}} seq_cst seq_cst
 }
+
+ulong dopopcount(ubyte n1, ushort n2, uint n3, ulong n4) {
+	return popCount(n1) + popCount(n2) + popCount(n3) + popCount(n4);
+// CHECK-LABEL: _D10intrinsics10dopopcountFMhtkmZm
+// CHECK: call i8 @llvm.ctpop.i8(i8 {{.*}})
+// CHECK: call i16 @llvm.ctpop.i16(i16 {{.*}})
+// CHECK: call i32 @llvm.ctpop.i32(i32 {{.*}})
+// CHECK: call i64 @llvm.ctpop.i64(i64 {{.*}})
+}
+
+ulong docountleadingzeros(ubyte n1, ushort n2, uint n3, ulong n4) {
+	auto a = countLeadingZeros(n1) + countLeadingZeros(n2);
+	auto b = countLeadingZeros(n3) + countLeadingZeros(n4);
+	return a + b;
+// CHECK-LABEL: _D10intrinsics19docountleadingzerosFMhtkmZm
+// CHECK: call i8 @llvm.ctlz.i8(i8 {{.*}})
+// CHECK: call i16 @llvm.ctlz.i16(i16 {{.*}})
+// CHECK: call i32 @llvm.ctlz.i32(i32 {{.*}})
+// CHECK: call i64 @llvm.ctlz.i64(i64 {{.*}})
+}
+
+ulong docounttrailingzeros(ubyte n1, ushort n2, uint n3, ulong n4) {
+	auto a = countTrailingZeros(n1) + countTrailingZeros(n2);
+	auto b = countTrailingZeros(n3) + countTrailingZeros(n4);
+	return a + b;
+// CHECK-LABEL: _D10intrinsics20docounttrailingzerosFMhtkmZm
+// CHECK: call i8 @llvm.cttz.i8(i8 {{.*}})
+// CHECK: call i16 @llvm.cttz.i16(i16 {{.*}})
+// CHECK: call i32 @llvm.cttz.i32(i32 {{.*}})
+// CHECK: call i64 @llvm.cttz.i64(i64 {{.*}})
+}
