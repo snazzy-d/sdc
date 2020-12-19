ecd75ba6ace970e4ab4dcff679909eca480b3128
Amaury Séchet <deadalnix@gmail.com>
2020-12-19 10:18:33 +0100

    Move to python3

--- test/llvm/runlit.py
+++ test/llvm/runlit.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 llvmconfig = os.getenv('LLVM_CONFIG', 'llvm-config')
 
 import subprocess
-llvm_source_root = subprocess.check_output([llvmconfig, "--src-root"]).strip()
+llvm_source_root = subprocess.check_output([llvmconfig, "--src-root"]).decode('utf-8').strip()
 
 # Make sure we can find the lit package.
 import sys
 sys.path.insert(0, os.path.join(llvm_source_root, 'utils', 'lit'))
 
 if __name__=='__main__':
-    import lit
-    lit.main()
+    from lit.main import main
+    main()
