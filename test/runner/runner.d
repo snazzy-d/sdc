6d20f740a7595912e602792358733ed472050b7a
Amaury Séchet <deadalnix@gmail.com>
2021-03-11 22:19:40 +0100

    Refactor runner.d

--- test/runner/runner.d
+++ test/runner/runner.d
@@ -157,43 +157,22 @@ int main(string[] args) {
 		"help", delegate {usage(); exit(0);},
 	);
 	
+	string[] tests;
+	
 	if (args.length > 1) {
-		int testNumber = to!int(args[1]);
-		auto testName = getTestFilename(testNumber);
-		auto job = spawn(&test, testName, compiler);
-		job.send(thisTid);
-
-		auto result = receiveOnly!(string, bool, bool)();
-		bool regressed = !result[1] & result[2];
-		bool fixed = result[1] & !result[2];
-
-		if (result[1]) {
-			if (!displayOnlyFailed) writef("%s: %s", result[0], "SUCCEEDED");
-		} else {
-			writef("%s: %s", result[0], "FAILED");
+		tests = args[1 .. $].map!(a => getTestFilename(to!int(a))).array();
+	} else {
+		// Figure out how many tests there are.
+		int testNumber = -1;
+		while (exists(getTestFilename(++testNumber))) {}
+		if (testNumber < 0) {
+			stderr.writeln("No tests found.");
+			return -1;
 		}
-
-		if (fixed) {
-			if (!displayOnlyFailed) writefln(", FIXED");
-		} else if (regressed) {
-			writeln(", REGRESSION");
-		} else {
-			writeln();
-		}
-
-		return regressed ? -1 : 0;
-	}
-
-	// Figure out how many tests there are.
-	int testNumber = -1;
-	while (exists(getTestFilename(++testNumber))) {}
-	if (testNumber < 0) {
-		stderr.writeln("No tests found.");
-		return -1;
+		
+		tests = iota(0, testNumber).map!getTestFilename().array();
 	}
 	
-	auto tests = iota(0, testNumber).map!getTestFilename().array();
-
 	size_t testIndex = 0;
 	int passed = 0;
 	int regressions = 0;
@@ -216,13 +195,17 @@ int main(string[] args) {
 			regressions += regressed;
 			improvments += fixed;
 			if (testResult[1]) {
-				if (!displayOnlyFailed) writef("%s: %s", testResult[0], "SUCCEEDED");
+				if (!displayOnlyFailed) {
+					writef("%s: %s", testResult[0], "SUCCEEDED");
+				}
 			} else {
 				writef("%s: %s", testResult[0], "FAILED");
 			}
 
 			if (fixed) {
-				if (!displayOnlyFailed) writefln(", FIXED");
+				if (!displayOnlyFailed) {
+					writefln(", FIXED");
+				}
 			} else if (regressed) {
 				writefln(", REGRESSION");
 			} else {
@@ -233,12 +216,12 @@ int main(string[] args) {
 		}
 	}
 
-	if (testNumber > 0) {
+	if (tests.length > 0) {
 		writefln("Summary: %s tests, %s pass%s, %s failure%s, %.2f%% pass rate, "
 				 ~ "%s regressions, %s improvements.",
-				 testNumber, passed, passed == 1 ? "" : "es",
-				 testNumber - passed, (testNumber - passed) == 1 ? "" : "s",
-				 (cast(real)passed / testNumber) * 100,
+				 tests.length, passed, passed == 1 ? "" : "es",
+				 tests.length - passed, (tests.length - passed) == 1 ? "" : "s",
+				 (cast(real)passed / tests.length) * 100,
 				 regressions, improvments);
 	}
 	
