ae94650a1dd669007708b4c7c0a7844975d608e9
Amaury Séchet <deadalnix@gmail.com>
2021-03-16 15:11:05 +0100

    Reflow comments in test0068.d

--- test/runner/test0068.d
+++ test/runner/test0068.d
@@ -3,57 +3,56 @@
 //T retval:6
 //? desc:Various cases for comments.
 
-int main()
-{
+int main() {
 //      return 21;
 
 // //   return 31;
 
-        string a = /+ "+/" +/ 1";
-        string c = /* "*/" */ 1";
-        int    d = 1 + /* 2 */ + /+ 3 +/ // 4
-                   + 5;
-/*
-        return 22;
-*/
+	string a = /+ "+/ " +/ 1";
+	string c = /* "*/ " */ 1";
+	int d = 1 + /* 2 */ + /+ 3 +/ // 4
+	+5;
+	/*
+	 * return 22;
+	 */
 
-/*      return 23; */
+	/* return 23; */
 
-/* /*   return 32; */
+	/* /* return 32; */
 
-/+
-        return 24;
-+/
+	/+
+	 + return 24;
+	 +/
 
-/+      return 25; +/
+	/+ return 25; +/
 
-/+ /+   return 33; +/ +/
+	/+ /+ return 33; +/ +/
 
-// /*   return 26; */
-// /+   return 27; */
+	// /* return 26; */
+	// /+ return 27; */
 
-/* //   return 28; */
-/* //   return 28; */
+	/* // return 28; */
+	/* // return 28; */
 
-/*
-     // return 29;
-*/
+	/*
+	// return 29;
+	*/
 
-/*
-     /* return 34;
-*/
+	/*
+	 * /* return 34;
+	 */
 
-/*
-     /+ return 35;
-*/
+	/*
+	 * /+ return 35;
+	 */
 
-/+
-     // return 30;   
-+/
+	/+
+	// return 30;
+	+/
 
-/+
-     /* return 36;
-+/
+	/+
+	 + /* return 36;
+	 +/
 
-        return d;
+	return d;
 }
