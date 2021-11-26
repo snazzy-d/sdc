4b63713f614d585308e824aa64b80332b696fed5
Amaury Séchet <deadalnix@gmail.com>
2021-11-26 17:40:28 +0000

    Add format tests for conditionals

---
+++ test/format/expression.d
@@ -0,0 +1,24 @@
+void fun() {
+	auto a = c ? x : y;
+	auto a = condition
+		? result_if_true
+		: result_if_false;
+
+	auto a =
+		condition
+				? nested_condition_1
+			? nested_result_1_1
+			: nested_result_1_2
+	: nested_condition_2
+		? nested_result_2_1
+		: nested_result_2_2;
+
+	auto a = (c
+			? long_result_1
+			: long_result_2) ? (a + b)
+		? long_result_3
+		: long_result_4
+	: (c + d)
+		? long_result_4
+		: long_result_6;
+}
