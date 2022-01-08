203deb20cfcdf1785c695b03ef0db258b43f5bf2
Amaury Séchet <deadalnix@gmail.com>
2022-01-08 00:33:39 +0000

    Add test case for template alias declaration

--- test/format/declaration.d
+++ test/format/declaration.d
@@ -2,3 +2,10 @@ template Identity(T)
 	if (SomeCondition!T) {
 	alias Identity = T;
 }
+
+alias TplAlias(
+	alias A, T = TT) = Tpl!(A, T);
+
+alias TplAlias(
+	alias A, T = DefaultType) =
+	Tpl!(A, T, false);
