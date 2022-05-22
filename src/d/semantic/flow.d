e0f7451b7f9c2d32ffea83c88a390f15d854270a
Amaury Séchet <deadalnix@gmail.com>
2022-05-22 22:15:25 +0000

    Format flow.d

--- src/d/semantic/flow.d
+++ src/d/semantic/flow.d
@@ -10,44 +10,44 @@ struct FlowAnalyzer {
 private:
 	SemanticPass pass;
 	alias pass this;
-	
+
 	Body fbody;
-	
+
 	import d.ir.symbol;
 	uint[Variable] closure;
 	uint nextClosureIndex;
-	
+
 public:
 	this(SemanticPass pass, Function f) in {
 		assert(f.fbody, "f does not have a body");
 	} do {
 		this.pass = pass;
-		
+
 		fbody = f.fbody;
 		nextClosureIndex = f.hasContext;
-		foreach(p; f.params) {
+		foreach (p; f.params) {
 			if (p.storage == Storage.Capture) {
 				assert(p !in closure);
 				closure[p] = nextClosureIndex++;
 			}
 		}
 	}
-	
+
 	uint[Variable] getClosure() {
-		foreach(b; range(fbody)) {
+		foreach (b; range(fbody)) {
 			visit(b);
 		}
-		
+
 		return closure;
 	}
-	
+
 	void visit(BasicBlockRef b) {
 		auto instructions = range(fbody, b);
-		foreach(i; instructions) {
+		foreach (i; instructions) {
 			if (i.op != OpCode.Alloca) {
 				continue;
 			}
-			
+
 			auto v = i.var;
 			if (v.storage == Storage.Capture) {
 				assert(v !in closure);
