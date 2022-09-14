483a6e5236a2793e639b9b45bbeb409da0807a0a
Amaury Séchet <deadalnix@gmail.com>
2022-09-14 17:13:21 +0000

    Add test cases for named imports

--- test/format/imports.d
+++ test/format/imports.d
@@ -30,3 +30,31 @@ public import foo.bar
 	: symbol1, symbol2;
 static import foo.bar
 	: symbol1, symbol2;
+import foo.bar : foo = bar, fizz = buzz;
+import foo.bar
+	: foo = bar, fizz = buzz, symbol1;
+import foo.bar : foo = bar, fizz = buzz,
+                 symbol1, symbol2;
+import foo.bar
+	: foo = bar, fizz = buzz, symbol1,
+	  symbol2, symbol3;
+import foo.bar
+	: foo = bar, fizz = buzz, symbol1,
+	  symbol2, symbol3, symbol4;
+import foo.bar :
+foo = bar,
+	fizz = buzz,
+	symbol1,
+	symbol2,
+	symbol3,
+	symbol4,
+	symbol5;
+import foo.bar :
+foo = bar,
+	fizz = buzz,
+	symbol1,
+	symbol2,
+	symbol3,
+	symbol4,
+	symbol5,
+	symbol6;
