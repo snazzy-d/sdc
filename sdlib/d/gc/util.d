11ebb453033c5a41aa09370e95871e1207547d56
Amaury Séchet <deadalnix@gmail.com>
2022-11-28 16:24:51 +0000

    Remove dead code in the GC

--- sdlib/d/gc/util.d
+++ sdlib/d/gc/util.d
@@ -1,20 +1,5 @@
 module d.gc.util;
 
-// XXX: Would require more benchmarks, but it looks like
-// it is faster to do it that way than using bsr/bsf ??!?
-auto pow2ceil(size_t x) {
-	x--;
-	x |= x >> 1;
-	x |= x >> 2;
-	x |= x >> 4;
-	x |= x >> 8;
-	x |= x >> 16;
-	x |= x >> 32;
-	x++;
-
-	return x;
-}
-
 size_t log2floor(size_t x) {
 	if (x == 0) {
 		return 0;
