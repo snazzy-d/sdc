71b982ee1258263697ec47e5fb3c4eb7b0a7bee9
Amaury Séchet <deadalnix@gmail.com>
2022-11-28 17:42:51 +0000

    Simplify the config.map code

---
+++ src/config/util.d
@@ -0,0 +1,23 @@
+module config.util;
+
+bool isPow2(uint x) {
+	return (x & (x - 1)) == 0;
+}
+
+// I have not figured out how to do this in a sensible way.
+// See: https://forum.dlang.org/post/zsaghidvbsdwqthadphx@forum.dlang.org
+ulong mulhi(ulong a, ulong b) {
+	version(LDC) {
+		import ldc.llvmasm;
+		return __ir!(`
+				%a = zext i64 %0 to i128
+				%b = zext i64 %1 to i128
+				%r = mul i128 %a, %b
+				%r2 = lshr i128 %r, 64
+				%r3 = trunc i128 %r2 to i64
+				ret i64 %r3`, ulong)(a, b);
+	} else {
+		// Not mulhi at all, but will do for now.
+		return h >> 28;
+	}
+}
--- src/source/swar/util.d
+++ src/source/swar/util.d
@@ -3,3 +3,7 @@ module source.swar.util;
 auto read(T)(string s) in(s.length >= T.sizeof) {
 	return *(cast(T*) s.ptr);
 }
+
+auto read(T)(const(ubyte)[] data) in(data.length >= T.sizeof) {
+	return *(cast(T*) data.ptr);
+}
