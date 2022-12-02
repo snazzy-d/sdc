be17fec93ebd7bb1d01b70624f422da90afa25f7
Amaury Séchet <deadalnix@gmail.com>
2022-12-02 15:53:21 +0000

    Add fallback for when packagers screw up src-dir .

--- test/llvm/runlit.py
+++ test/llvm/runlit.py
@@ -4,11 +4,15 @@ import os
 llvmconfig = os.getenv('LLVM_CONFIG', 'llvm-config')
 
 import subprocess
-llvm_source_root = subprocess.check_output([llvmconfig, "--src-root"]).decode('utf-8').strip()
+import sys
 
 # Make sure we can find the lit package.
-import sys
+llvm_source_root = subprocess.check_output([llvmconfig, "--src-root"]).decode('utf-8').strip()
 sys.path.insert(0, os.path.join(llvm_source_root, 'utils', 'lit'))
+
+# src-root mayb be bogous with some packaging of LLVM, so we use obj-root as a fallback.
+llvm_obj_root = subprocess.check_output([llvmconfig, "--obj-root"]).decode('utf-8').strip()
+sys.path.insert(0, os.path.join(llvm_obj_root, 'build', 'utils', 'lit'))
 
 if __name__=='__main__':
     from lit.main import main
