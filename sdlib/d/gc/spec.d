2a0e4e718b0c8ad09ef4c6d7ff8ad4a2cdaa1073
Amaury Séchet <deadalnix@gmail.com>
2023-02-11 15:12:15 +0000

    Add constants about pointer size and address space in gc.spec

--- sdlib/d/gc/spec.d
+++ sdlib/d/gc/spec.d
@@ -1,8 +1,18 @@
 module d.gc.spec;
 
+// Pointer size.
+enum LgPointerSize = 8 * size_t.sizeof;
+enum PointerSize = 1UL << LgPointerSize;
+
+// Page size.
 enum LgPageSize = 12;
 enum PageSize = 1UL << LgPageSize;
 enum PageMask = PageSize - 1;
+
+// Virtual address space size.
+enum LgAddressSpace = 48;
+enum AddressSpace = 1UL << LgAddressSpace;
+enum AddressMask = AddressSpace - 1;
 
 // A chunk is 1024 pages.
 enum LgChunkPageCount = 10;
