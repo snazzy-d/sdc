1fa613f51b4a04955e1a20a9870177a3ae633686
Amaury Séchet <deadalnix@gmail.com>
2023-02-12 18:31:14 +0000

    Add a base allocator to request address space from the system.

---
+++ sdlib/d/gc/base.d
@@ -0,0 +1,231 @@
+module d.gc.base;
+
+import d.gc.extent;
+import d.gc.sizeclass;
+import d.gc.spec;
+import d.gc.util;
+
+struct Block {
+	size_t size;
+	Block* next;
+
+	Extent extent;
+}
+
+/**
+ * Bump the pointer style allocator.
+ *
+ * It never deallocates, except on destruction.
+ * It serves as a base allocator for address space.
+ *
+ * Note: We waste a fair amount of address space when
+ * allocating with large alignment constraints. This is
+ * not a huge deal per se because there is no memory
+ * actually backing this address space, but this might
+ * lead to various ineffisciencies.
+ */
+struct Base {
+private:
+	import d.sync.mutex;
+	Mutex mutex;
+
+	import d.gc.arena;
+	Arena* arena;
+
+	/**
+	 * In order to avoid address space fragmentation,
+	 * we allocate larger and larger blocks of addresses.
+	 *
+	 * We do so by remembering the size class we used and
+	 * bump by 1. This ensure the block size we allocate
+	 * grows exponentially.
+	 */
+	ubyte lastSizeClass;
+
+	// Serial number generation?
+	size_t nextSerialNumber;
+
+	// Linked list of all the blocks.
+	Block* head;
+
+	// Free extents we can allocate to arenas.
+	import d.gc.rbtree, d.gc.extent;
+	RBTree!(Extent, sizeAddrExtentCmp) availableExtents;
+
+	// TODO: Keep track of stats.
+	// TODO: Support transparent huge pages?
+
+public:
+	void clear() shared {
+		(cast(Base*) &this).clearImpl();
+	}
+
+	void* alloc(size_t size, size_t alignment) shared {
+		return (cast(Base*) &this).allocImpl(size, alignment);
+	}
+
+private:
+	void clearImpl() {
+		mutex.lock();
+		scope(exit) mutex.unlock();
+
+		auto next = head;
+		while (next !is null) {
+			auto block = next;
+			next = block.next;
+
+			import d.gc.pages;
+			pages_unmap(block, block.size);
+		}
+	}
+
+	void* allocImpl(size_t size, size_t alignment) {
+		alignment = alignUp(alignment, Quantum);
+		size = alignUp(size, alignment);
+		auto sc = getSizeClass(size + alignment - Quantum);
+
+		mutex.lock();
+		scope(exit) mutex.unlock();
+
+		auto extent = availableExtents.extractBestFit(cast(Extent*) sc);
+		if (extent is null) {
+			extent = extentAlloc(size, alignment);
+		}
+
+		if (extent is null) {
+			return null;
+		}
+
+		return extentBumpAlloc(extent, size, alignment);
+	}
+
+	Extent* extentAlloc(size_t size, size_t alignment) {
+		assert(mutex.isHeld(), "Mutex not held!");
+		assert(isAligned(alignment, Quantum), "Invalid alignement!");
+		assert(isAligned(size, alignment), "Invalid size!");
+
+		auto block = blockAlloc(size, alignment, lastSizeClass);
+		if (block is null) {
+			return null;
+		}
+
+		// Keep track of the last block size.
+		lastSizeClass = getSizeClass(block.size);
+
+		// Serial number.
+		block.extent.serialNumber = nextSerialNumber++;
+
+		// Add the newly allocated block to the list of blocks.
+		block.next = head;
+		head = block;
+
+		return &block.extent;
+	}
+
+	void* extentBumpAlloc(Extent* extent, size_t size, size_t alignment) {
+		assert(mutex.isHeld(), "Mutex not held!");
+		assert(isAligned(alignment, Quantum), "Invalid alignement!");
+		assert(isAligned(size, alignment), "Invalid size!");
+
+		auto p = cast(size_t) extent.addr;
+		auto gap = alignUp(p, alignment) - p;
+		auto ret = extent.addr + gap;
+
+		assert(extent.size >= size + gap, "Insufiscient space in the Extent!");
+		extent.addr = ret + size;
+		extent.size -= gap + size;
+
+		availableExtents.insert(extent);
+		return ret;
+	}
+
+	Block* blockAlloc(size_t size, size_t alignment, ubyte lastSizeClass) {
+		assert(mutex.isHeld(), "Mutex not held!");
+		assert(isAligned(alignment, Quantum), "Invalid alignement!");
+		assert(isAligned(size, alignment), "Invalid size!");
+
+		mutex.unlock();
+		scope(exit) mutex.lock();
+
+		auto headerSize = alignUp(Block.sizeof, alignment);
+
+		/**
+		 * We make sure we allocate at least a huge page, to leave the
+		 * kernel the opportunity to use huge pages.
+		 *
+		 * We also increase the size of the block exponentially by bumping
+		 * to the next size class if apropriate. This ensures we do not
+		 * fragment the address space more than necessary and limit degenerate
+		 * cases where we call into the base allocator again and again.
+		 */
+		auto minBlockSize =
+			alignUp(getAllocSize(headerSize + size), HugePageSize);
+		auto nextSizeClass =
+			lastSizeClass + (lastSizeClass < ClassCount.Total - 1);
+		auto nextBlockSize = getSizeFromBinID(nextSizeClass);
+		auto blockSize =
+			(nextBlockSize < minBlockSize) ? minBlockSize : nextBlockSize;
+
+		import d.gc.pages;
+		auto block = cast(Block*) pages_map(null, blockSize, HugePageSize);
+		if (block is null) {
+			return null;
+		}
+
+		block.size = blockSize;
+		block.extent.addr = (cast(void*) block) + headerSize;
+		block.extent.size = blockSize - headerSize;
+
+		return block;
+	}
+}
+
+unittest base_alloc {
+	shared Base base;
+	scope(exit) base.clear();
+
+	auto getBlockCount(shared ref Base base) {
+		size_t count = 0;
+
+		auto next = base.head;
+		while (next !is null) {
+			count++;
+			next = next.next;
+		}
+
+		return count;
+	}
+
+	assert(getBlockCount(base) == 0);
+
+	auto ptr0 = base.alloc(5, 1);
+	assert(getBlockCount(base) == 1);
+	assert(base.head.size == HugePageSize);
+	assert(isAligned(ptr0, Quantum));
+
+	auto ptr1 = base.alloc(3, 1);
+	assert(getBlockCount(base) == 1);
+	assert(base.head.size == HugePageSize);
+	assert(isAligned(ptr1, Quantum));
+
+	// Check large alignment.
+	auto ptr3 = base.alloc(HugePageSize, HugePageSize);
+	assert(getBlockCount(base) == 2);
+	assert(base.head.size == 2 * HugePageSize);
+	assert(isAligned(ptr3, HugePageSize));
+
+	// Check that the block we allocate grow exponentially.
+	auto ptr4 = base.alloc(HugePageSize, HugePageSize);
+	assert(getBlockCount(base) == 3);
+	assert(base.head.size == 5 * (HugePageSize / 2));
+	assert(isAligned(ptr4, HugePageSize));
+
+	// Reuse existing blocks.
+	auto ptr5 = base.alloc(HugePageSize / 2, 1);
+	assert(getBlockCount(base) == 3);
+	assert(isAligned(ptr5, Quantum));
+
+	auto ptr6 = base.alloc(HugePageSize / 2, 1);
+	assert(getBlockCount(base) == 3);
+	assert(isAligned(ptr6, Quantum));
+}
--- sdlib/d/gc/extent.d
+++ sdlib/d/gc/extent.d
@@ -1,17 +1,28 @@
 module d.gc.extent;
 
 import d.gc.rbtree;
+import d.gc.sizeclass;
+
 alias ExtentTree = RBTree!(Extent, addrRangeExtentCmp);
 
 struct Extent {
 	import d.gc.arena;
 	Arena* arena;
 
+	void* addr;
+	size_t size;
+
+	// TODO: hpdata?
+
+	// Not necesserly unique, as splitting an Extent
+	// Will create two extent with the same serial.
+	size_t serialNumber;
+
+	// TODO: Various links for tree, heaps, etc...
 	import d.gc.rbtree;
 	ExtentTree.Node node;
 
-	void* addr;
-	size_t size;
+	// TODO: slab data and/or stats.
 }
 
 ptrdiff_t addrExtentCmp(Extent* lhs, Extent* rhs) {
@@ -29,4 +40,28 @@ ptrdiff_t addrRangeExtentCmp(Extent* lhs, Extent* rhs) {
 
 	// We need to compare that way to avoid integer overflow.
 	return (l >= rend) - (l < rstart);
+}
+
+ptrdiff_t sizeAddrExtentCmp(Extent* lhs, Extent* rhs) {
+	auto rAddr = cast(size_t) rhs.addr;
+	int rBinID = getBinID(rhs.size + 1) - 1;
+
+	int lBinID;
+	size_t lAddr;
+	auto l = cast(size_t) lhs;
+
+	import d.gc.spec;
+	if (l & ~PageMask) {
+		lAddr = cast(size_t) lhs.addr;
+		lBinID = getBinID(lhs.size + 1) - 1;
+	} else {
+		lAddr = 0;
+		lBinID = cast(int) (l & PageMask);
+	}
+
+	if (lBinID != rBinID) {
+		return lBinID - rBinID;
+	}
+
+	return (lAddr > rAddr) - (lAddr < rAddr);
 }
