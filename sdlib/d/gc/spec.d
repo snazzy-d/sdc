74d0f3786e32d24d6e9a859e0f4b6ae8568640c2
Amaury Séchet <deadalnix@gmail.com>
2023-02-13 16:06:34 +0000

    Fix pointer size in gc.spec

--- sdlib/d/gc/spec.d
+++ sdlib/d/gc/spec.d
@@ -1,8 +1,25 @@
 module d.gc.spec;
 
+import d.gc.util;
+
 // Pointer size.
-enum LgPointerSize = 8 * size_t.sizeof;
+enum LgPointerSize = log2floor(size_t.sizeof);
 enum PointerSize = 1UL << LgPointerSize;
+
+static assert(PointerSize == size_t.sizeof, "Invalid pointer size!");
+
+// We want 64-bits aligned allocations.
+enum LgQuantum = 3;
+enum Quantum = 1UL << LgQuantum;
+enum QuantumMask = Quantum - 1;
+
+static assert(Quantum >= PointerSize,
+              "Quantum must be at least pointer sized!");
+
+// Cache line.
+enum LgCacheLine = 6;
+enum CacheLine = 1UL << LgCacheLine;
+enum CacheLineMask = CacheLine - 1;
 
 // Page size.
 enum LgPageSize = 12;
@@ -25,8 +42,3 @@ enum ChunkPageCount = 1UL << LgChunkPageCount;
 enum LgChunkSize = LgPageSize + LgChunkPageCount;
 enum ChunkSize = 1UL << LgChunkSize;
 enum ChunkAlignMask = ChunkSize - 1;
-
-// We want 64-bits aligned allocations.
-enum LgQuantum = 3;
-enum Quantum = 1 << LgQuantum;
-enum QuantumMask = Quantum - 1;
--- sdlib/d/gc/util.d
+++ sdlib/d/gc/util.d
@@ -1,5 +1,29 @@
 module d.gc.util;
 
+size_t log2floor(size_t x) {
+	if (x == 0) {
+		return 0;
+	}
+
+	import sdc.intrinsics;
+	enum S = size_t.sizeof * 8;
+	return S - countLeadingZeros(x) - 1;
+}
+
+unittest log2floor {
+	assert(log2floor(0) == 0);
+	assert(log2floor(1) == 0);
+	assert(log2floor(2) == 1);
+	assert(log2floor(3) == 1);
+	assert(log2floor(4) == 2);
+	assert(log2floor(7) == 2);
+	assert(log2floor(8) == 3);
+	assert(log2floor(15) == 3);
+	assert(log2floor(16) == 4);
+	assert(log2floor(31) == 4);
+	assert(log2floor(32) == 5);
+}
+
 bool isPow2(size_t x) {
 	return (x & (x - 1)) == 0;
 }
@@ -217,28 +241,4 @@ unittest alignUp {
 			testAlignUp(s, a, a);
 		}
 	}
-}
-
-size_t log2floor(size_t x) {
-	if (x == 0) {
-		return 0;
-	}
-
-	import sdc.intrinsics;
-	enum S = size_t.sizeof * 8;
-	return S - countLeadingZeros(x) - 1;
-}
-
-unittest log2floor {
-	assert(log2floor(0) == 0);
-	assert(log2floor(1) == 0);
-	assert(log2floor(2) == 1);
-	assert(log2floor(3) == 1);
-	assert(log2floor(4) == 2);
-	assert(log2floor(7) == 2);
-	assert(log2floor(8) == 3);
-	assert(log2floor(15) == 3);
-	assert(log2floor(16) == 4);
-	assert(log2floor(31) == 4);
-	assert(log2floor(32) == 5);
 }
