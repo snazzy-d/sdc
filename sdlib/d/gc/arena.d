d19946f127295bc00b95916e1a5e658938fe10b8
Amaury Séchet <deadalnix@gmail.com>
2023-04-13 19:07:17 +0000

    Remove the old GC machinery to mark&sweep

--- sdlib/d/gc/arena.d
+++ sdlib/d/gc/arena.d
@@ -57,31 +57,6 @@ struct Arena {
 		return a;
 	}
 
-	/**
-	 * Legacy Arena.
-	 *
-	 * The arena is being migrated from being thread unsafe
-	 * and use the Chunk based allocator to being thread safe
-	 * and use the huge page allocator.
-	 *
-	 * In order to keep everythign working as we go, the legacy
-	 * mechanism are left as this and the code migrated path by path.
-	 * Once everything is migrated, everythign in that section
-	 * will be removed.
-	 */
-	import d.sync.mutex;
-	shared Mutex chunkMutex;
-
-	// Set of chunks for GC lookup.
-	ChunkSet chunkSet;
-
-	// Extent describing huge allocs.
-	shared Mutex hugeMutex;
-
-	import d.gc.extent;
-	ExtentTree hugeTree;
-	ExtentTree hugeLookupTree;
-
 	const(void*)* stackBottom;
 	const(void*)[][] roots;
 
@@ -209,25 +184,6 @@ private:
 	}
 
 	/**
-	 * Huge alloc/free facilities.
-	 */
-	void freeExtent(Extent* e) {
-		// FIXME: in contract
-		assert(e !is null);
-		assert(hugeMutex.isHeld(), "Mutex not held!");
-		assert(hugeTree.find(e) is null);
-		assert(hugeLookupTree.find(e) is null);
-
-		hugeMutex.unlock();
-		scope(exit) hugeMutex.lock();
-
-		import d.gc.pages;
-		pages_unmap(e.addr, e.size);
-
-		free(e);
-	}
-
-	/**
 	 * GC facilities
 	 */
 	void addRoots(const void[] range) {
@@ -246,36 +202,8 @@ private:
 	}
 
 	void collect() {
-		{
-			hugeMutex.lock();
-			scope(exit) hugeMutex.unlock();
-
-			// Get ready to detect huge allocations.
-			hugeLookupTree = hugeTree;
-
-			// FIXME: This bypass visibility.
-			hugeTree.root = null;
-		}
-
 		// TODO: The set need a range interface or some other way to iterrate.
-		auto chunks = chunkSet.cloneChunks();
-		foreach (c; chunks) {
-			if (c is null) {
-				continue;
-			}
-
-			c.prepare();
-		}
-
-		// Mark bitmap as live.
-		foreach (c; chunks) {
-			if (c is null) {
-				continue;
-			}
-
-			auto bmp = cast(void**) &c.header.bitmap;
-			scan(bmp[0 .. 1]);
-		}
+		// FIXME: Prepare the GC so it has bitfields for all extent classes.
 
 		// Scan the roots !
 		__sdgc_push_registers(scanStack);
@@ -283,35 +211,9 @@ private:
 			scan(range);
 		}
 
-		// Go on and on until all worklists are empty.
-		auto needRescan = true;
-		while (needRescan) {
-			needRescan = false;
-			foreach (c; chunks) {
-				if (c is null) {
-					continue;
-				}
+		// TODO: Go on and on until all worklists are empty.
 
-				needRescan = c.scan() || needRescan;
-			}
-		}
-
-		// Now we can collect.
-		foreach (c; chunks) {
-			if (c is null) {
-				continue;
-			}
-
-			c.collect();
-		}
-
-		hugeMutex.lock();
-		scope(exit) hugeMutex.unlock();
-
-		// Extents that have not been moved to hugeTree are dead.
-		while (!hugeLookupTree.empty) {
-			freeExtent(hugeLookupTree.extractAny());
-		}
+		// TODO: Collect.
 	}
 
 	bool scanStack() {
@@ -328,30 +230,26 @@ private:
 	bool scan(const(void*)[] range) {
 		bool newPtr;
 		foreach (ptr; range) {
+			enum PtrMask = ~(AddressSpace - 1);
 			auto iptr = cast(size_t) ptr;
 
-			import d.gc.chunk;
-			auto c = findChunk(ptr);
-			if (c !is null && chunkSet.test(c)) {
-				newPtr = c.mark(ptr) || newPtr;
+			if (iptr & PtrMask) {
+				// This is not a pointer, move along.
+				// TODO: Replace this with a min-max test.
 				continue;
 			}
 
-			Extent ecmp;
-			ecmp.addr = cast(void*) ptr;
-			auto e = hugeLookupTree.extract(&ecmp);
-			if (e is null) {
+			import d.gc.util;
+			auto aptr = alignDown(ptr, PageSize);
+
+			auto a = allocator;
+			auto pd = a.emap.lookup(aptr);
+			if (pd.extent is null) {
+				// We have no mappign there.
 				continue;
 			}
 
-			hugeTree.insert(e);
-
-			auto hugeRange = makeRange(e.addr[0 .. e.size]);
-
-			// FIXME: Ideally, a worklist is preferable as
-			// 1/ We could recurse a lot this way.
-			// 2/ We want to keep working on the same chunk for locality.
-			scan(hugeRange);
+			// FIXME: We have something, mark!
 		}
 
 		return newPtr;
@@ -376,160 +274,6 @@ const(void*)[] makeRange(const void[] range) {
 
 	auto length = (range.length - aiptr + iptr) / size_t.sizeof;
 	return aptr[0 .. length];
-}
-
-struct ChunkSet {
-	// Set of chunks for GC lookup.
-	Chunk** chunks;
-	uint chunkCount;
-
-	// Metadatas for the chunk set.
-	ubyte lgChunkSetSize;
-	ubyte maxProbe;
-
-	@property
-	Arena* arena() {
-		// We can find the arena from this pointer.
-		auto chunkSetOffset = cast(size_t) (&(cast(Arena*) null).chunkSet);
-		return cast(Arena*) ((cast(size_t) &this) - chunkSetOffset);
-	}
-
-	import d.gc.chunk;
-	bool register(Chunk* c) {
-		// FIXME: in contract
-		assert(!test(c));
-
-		// We resize if the set is 7/8 full.
-		auto limitSize = (7UL << lgChunkSetSize) / 8;
-		if (limitSize <= chunkCount) {
-			if (increaseSize()) {
-				return true;
-			}
-		}
-
-		chunkCount++;
-		insert(c);
-
-		// FIXME: out contract
-		assert(test(c));
-		return false;
-	}
-
-	/**
-	 * GC facilities
-	 */
-	bool test(Chunk* c) {
-		// FIXME: in contract
-		assert(c !is null);
-
-		auto k = (cast(size_t) c) >> LgChunkSize;
-		auto mask = (1 << lgChunkSetSize) - 1;
-
-		foreach (i; 0 .. maxProbe) {
-			if (c is chunks[(k + i) & mask]) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	Chunk*[] cloneChunks() {
-		if (chunks is null) {
-			return [];
-		}
-
-		auto oldLgChunkSetSize = lgChunkSetSize;
-		auto allocSize = size_t.sizeof << lgChunkSetSize;
-		auto buf = cast(Chunk**) arena.alloc(allocSize);
-
-		// We may have created a new chunk to allocate the buffer.
-		while (lgChunkSetSize != oldLgChunkSetSize) {
-			// If don't think this can run more than once,
-			// but better safe than sorry.
-			oldLgChunkSetSize = lgChunkSetSize;
-			allocSize = size_t.sizeof << lgChunkSetSize;
-			buf = cast(Chunk**) arena.realloc(buf, allocSize);
-		}
-
-		memcpy(buf, chunks, allocSize);
-		return buf[0 .. 1UL << lgChunkSetSize];
-	}
-
-private:
-	/**
-	 * Internal facilities
-	 */
-	void insert(Chunk* c) {
-		auto mask = (1 << lgChunkSetSize) - 1;
-
-		auto k = (cast(size_t) c) >> LgChunkSize;
-		auto p = (cast(uint) k) & mask;
-
-		auto i = p;
-		ubyte d = 0;
-		while (chunks[i] !is null) {
-			auto ce = chunks[i];
-			auto ke = (cast(size_t) ce) >> LgChunkSize;
-			auto pe = (cast(uint) ke) & mask;
-
-			// Robin hood hashing.
-			if (d > (i - pe)) {
-				chunks[i] = c;
-				c = ce;
-				k = ke;
-				p = pe;
-
-				if (d >= maxProbe) {
-					maxProbe = cast(ubyte) (d + 1);
-				}
-			}
-
-			i = ((i + 1) & mask);
-			d = cast(ubyte) (i - p);
-		}
-
-		chunks[i] = c;
-		if (d >= maxProbe) {
-			maxProbe = cast(ubyte) (d + 1);
-		}
-	}
-
-	bool increaseSize() {
-		auto oldChunks = chunks;
-		auto oldChunkSetSize = 1UL << lgChunkSetSize;
-
-		lgChunkSetSize++;
-		assert(lgChunkSetSize <= 32);
-
-		// auto newChunks = cast(Chunk**) arena.calloc(Chunk*.sizeof << lgChunkSetSize);
-		auto newChunks =
-			cast(Chunk**) arena.calloc(size_t.sizeof << lgChunkSetSize);
-		assert(oldChunks is chunks);
-
-		if (newChunks is null) {
-			return true;
-		}
-
-		maxProbe = 0;
-		chunks = newChunks;
-		auto rem = chunkCount;
-
-		for (uint i = 0; rem != 0; i++) {
-			assert(i < oldChunkSetSize);
-
-			auto c = oldChunks[i];
-			if (c is null) {
-				continue;
-			}
-
-			insert(c);
-			rem--;
-		}
-
-		arena.free(oldChunks);
-		return false;
-	}
 }
 
 extern(C):
