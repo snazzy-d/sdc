dff86bed8780740304615f72097ec1a6454f48c9
Amaury Séchet <deadalnix@gmail.com>
2023-04-17 19:26:24 +0000

    Make d.gc.spec size_t instead of ulong.

--- sdlib/d/gc/spec.d
+++ sdlib/d/gc/spec.d
@@ -2,15 +2,18 @@ module d.gc.spec;
 
 import d.gc.util;
 
+// Pointer sized one.
+enum size_t One = 1;
+
 // Pointer size.
 enum LgPointerSize = log2floor(size_t.sizeof);
-enum PointerSize = 1UL << LgPointerSize;
+enum PointerSize = One << LgPointerSize;
 
 static assert(PointerSize == size_t.sizeof, "Invalid pointer size!");
 
 // We want 64-bits aligned allocations.
 enum LgQuantum = 3;
-enum Quantum = 1UL << LgQuantum;
+enum Quantum = One << LgQuantum;
 enum QuantumMask = Quantum - 1;
 
 static assert(Quantum >= PointerSize,
@@ -18,20 +21,20 @@ static assert(Quantum >= PointerSize,
 
 // Cache line.
 enum LgCacheLine = 6;
-enum CacheLine = 1UL << LgCacheLine;
+enum CacheLine = One << LgCacheLine;
 enum CacheLineMask = CacheLine - 1;
 
 // Page size.
 enum LgPageSize = 12;
-enum PageSize = 1UL << LgPageSize;
+enum PageSize = One << LgPageSize;
 enum PageMask = PageSize - 1;
 
 // Huge page size.
 enum LgHugePageSize = 21;
-enum HugePageSize = 1UL << LgHugePageSize;
+enum HugePageSize = One << LgHugePageSize;
 enum HugePageMask = HugePageSize - 1;
 
 // Virtual address space.
 enum LgAddressSpace = 48;
-enum AddressSpace = 1UL << LgAddressSpace;
+enum AddressSpace = One << LgAddressSpace;
 enum AddressMask = AddressSpace - 1;
