b8737cad25fe654e1e83c496d4c9d293090ccf43
Amaury Séchet <deadalnix@gmail.com>
2023-04-17 12:42:02 +0000

    Add a thread local component to the GC.

--- sdlib/d/gc/arena.d
+++ sdlib/d/gc/arena.d
@@ -1,38 +1,10 @@
 module d.gc.arena;
 
+import d.gc.sizeclass;
 import d.gc.spec;
 import d.gc.emap;
 
-extern(C) void* __sd_gc_tl_malloc(size_t size) {
-	return tl.alloc(size);
-}
-
-extern(C) void* __sd_gc_tl_array_alloc(size_t size) {
-	return __sd_gc_tl_malloc(size);
-}
-
-extern(C) void _tl_gc_free(void* ptr) {
-	tl.free(ptr);
-}
-
-extern(C) void* _tl_gc_realloc(void* ptr, size_t size) {
-	return tl.realloc(ptr, size);
-}
-
-extern(C) void _tl_gc_set_stack_bottom(const void* bottom) {
-	// tl.stackBottom = makeRange(bottom[]).ptr;
-	// tl.stackBottom = makeRange(bottom[0 .. 0]).ptr;
-}
-
-extern(C) void _tl_gc_add_roots(const void[] range) {
-	(cast(Arena*) &tl).addRoots(range);
-}
-
-extern(C) void _tl_gc_collect() {
-	// tl.collect();
-}
-
-shared Arena tl;
+shared Arena gArena;
 
 struct Arena {
 	import d.gc.base;
@@ -56,86 +28,9 @@ struct Arena {
 		return a;
 	}
 
-	// const(void*)* stackBottom;
-	const(void*)[][] roots;
-
-	import d.gc.bin, d.gc.sizeclass;
+	import d.gc.bin;
 	Bin[ClassCount.Small] bins;
 
-	void* alloc(size_t size) shared {
-		if (size <= SizeClass.Small) {
-			return allocSmall(size);
-		}
-
-		return allocLarge(size, false);
-	}
-
-	void* calloc(size_t size) shared {
-		if (size <= SizeClass.Small) {
-			auto ret = allocSmall(size);
-			memset(ret, 0, size);
-			return ret;
-		}
-
-		return allocLarge(size, true);
-	}
-
-	void free(void* ptr) shared {
-		if (ptr is null) {
-			return;
-		}
-
-		auto pd = getPageDescriptor(ptr);
-		free(pd, ptr);
-	}
-
-	void* realloc(void* ptr, size_t size) shared {
-		if (size == 0) {
-			free(ptr);
-			return null;
-		}
-
-		if (ptr is null) {
-			return alloc(size);
-		}
-
-		auto copySize = size;
-		auto pd = getPageDescriptor(ptr);
-
-		if (pd.isSlab()) {
-			auto newSizeClass = getSizeClass(size);
-			auto oldSizeClass = pd.sizeClass;
-			if (newSizeClass == oldSizeClass) {
-				return ptr;
-			}
-
-			if (newSizeClass > oldSizeClass) {
-				copySize = getSizeFromClass(oldSizeClass);
-			}
-		} else {
-			auto esize = pd.extent.size;
-
-			import d.gc.util;
-			if (alignUp(size, PageSize) == esize) {
-				return ptr;
-			}
-
-			// TODO: Try to extend/shrink in place.
-			copySize = min(size, esize);
-		}
-
-		auto newPtr = alloc(size);
-		if (newPtr is null) {
-			return null;
-		}
-
-		memcpy(newPtr, ptr, copySize);
-		free(pd, ptr);
-
-		return newPtr;
-	}
-
-private:
 	/**
 	 * Small allocation facilities.
 	 */
@@ -178,125 +73,4 @@ private:
 			allocator.freePages(pd.extent);
 		}
 	}
-
-	/**
-	 * Utility functions.
-	 */
-	auto getPageDescriptor(void* ptr) shared {
-		import d.gc.util;
-		auto aptr = alignDown(ptr, PageSize);
-
-		auto a = allocator;
-		auto pd = a.emap.lookup(aptr);
-		assert(pd.extent !is null);
-		assert(pd.isSlab() || ptr is pd.extent.addr);
-
-		return pd;
-	}
-
-	/**
-	 * GC facilities
-	 */
-	void addRoots(const void[] range) {
-		// FIXME: Casting to void* is aparently not handled properly :(
-		auto ptr = cast(void*) roots.ptr;
-
-		// We realloc everytime. It doesn't really matter at this point.
-		roots.ptr = cast(const(void*)[]*) (cast(shared(Arena)*) &this)
-			.realloc(ptr, (roots.length + 1) * void*[].sizeof);
-
-		// Using .ptr to bypass bound checking.
-		roots.ptr[roots.length] = makeRange(range);
-
-		// Update the range.
-		roots = roots.ptr[0 .. roots.length + 1];
-	}
-
-	void collect() {
-		// TODO: The set need a range interface or some other way to iterrate.
-		// FIXME: Prepare the GC so it has bitfields for all extent classes.
-
-		// Scan the roots !
-		__sdgc_push_registers(scanStack);
-		foreach (range; roots) {
-			scan(range);
-		}
-
-		// TODO: Go on and on until all worklists are empty.
-
-		// TODO: Collect.
-	}
-
-	bool scanStack() {
-		const(void*) p;
-
-		auto iptr = cast(size_t) &p;
-		// auto iend = cast(size_t) stackBottom;
-		size_t iend = 0;
-		auto length = (iend - iptr) / size_t.sizeof;
-
-		auto range = (&p)[1 .. length];
-		return scan(range);
-	}
-
-	bool scan(const(void*)[] range) {
-		/*
-		bool newPtr;
-		foreach (ptr; range) {
-			enum PtrMask = ~(AddressSpace - 1);
-			auto iptr = cast(size_t) ptr;
-
-			if (iptr & PtrMask) {
-				// This is not a pointer, move along.
-				// TODO: Replace this with a min-max test.
-				continue;
-			}
-
-			import d.gc.util;
-			auto aptr = alignDown(ptr, PageSize);
-
-			auto a = allocator;
-			auto pd = a.emap.lookup(aptr);
-			if (pd.extent is null) {
-				// We have no mappign there.
-				continue;
-			}
-
-			// FIXME: We have something, mark!
-		}
-
-		return newPtr;
-		/*/
-		return false;
-		// */
-	}
-}
-
-private:
-
-/**
- * This function get a void[] range and chnage it into a
- * const(void*)[] one, reducing to alignement boundaries.
- */
-const(void*)[] makeRange(const void[] range) {
-	size_t iptr = cast(size_t) range.ptr;
-	auto aiptr = (((iptr - 1) / size_t.sizeof) + 1) * size_t.sizeof;
-
-	// Align the ptr and remove the differnece from the length.
-	auto aptr = cast(const(void*)*) aiptr;
-	if (range.length < 8) {
-		return aptr[0 .. 0];
-	}
-
-	auto length = (range.length - aiptr + iptr) / size_t.sizeof;
-	return aptr[0 .. length];
-}
-
-extern(C):
-version(OSX) {
-	// For some reason OSX's symbol get a _ prepended.
-	bool _sdgc_push_registers(bool delegate());
-	alias __sdgc_push_registers = _sdgc_push_registers;
-} else {
-	bool __sdgc_push_registers(bool delegate());
 }
---
+++ sdlib/d/gc/tl.d
@@ -0,0 +1,277 @@
+module d.gc.tl;
+
+import d.gc.sizeclass;
+import d.gc.spec;
+import d.gc.util;
+
+extern(C) void* __sd_gc_tl_malloc(size_t size) {
+	return tc.alloc(size);
+}
+
+extern(C) void* __sd_gc_tl_array_alloc(size_t size) {
+	return __sd_gc_tl_malloc(size);
+}
+
+extern(C) void _tl_gc_free(void* ptr) {
+	tc.free(ptr);
+}
+
+extern(C) void* _tl_gc_realloc(void* ptr, size_t size) {
+	return tc.realloc(ptr, size);
+}
+
+extern(C) void _tl_gc_set_stack_bottom(const void* bottom) {
+	tc.stackBottom = makeRange(bottom[0 .. 0]).ptr;
+}
+
+extern(C) void _tl_gc_add_roots(const void[] range) {
+	tc.addRoots(range);
+}
+
+extern(C) void _tl_gc_collect() {
+	tc.collect();
+}
+
+ThreadCache tc;
+
+struct ThreadCache {
+private:
+	import d.gc.arena;
+	shared(Arena)* _arena;
+
+	@property
+	shared(Arena)* arena() {
+		import sdc.intrinsics;
+		if (unlikely(_arena is null)) {
+			_arena = &gArena;
+		}
+
+		return _arena;
+	}
+
+	const(void)* stackBottom;
+	const(void*)[][] roots;
+
+public:
+	void* alloc(size_t size) {
+		if (size <= SizeClass.Small) {
+			return arena.allocSmall(size);
+		}
+
+		return arena.allocLarge(size, false);
+	}
+
+	void* calloc(size_t size) {
+		if (size <= SizeClass.Small) {
+			auto ret = arena.allocSmall(size);
+			memset(ret, 0, size);
+			return ret;
+		}
+
+		return arena.allocLarge(size, true);
+	}
+
+	void free(void* ptr) {
+		if (ptr is null) {
+			return;
+		}
+
+		auto pd = getPageDescriptor(ptr);
+		pd.extent.arena.free(pd, ptr);
+	}
+
+	void* realloc(void* ptr, size_t size) {
+		if (size == 0) {
+			free(ptr);
+			return null;
+		}
+
+		if (ptr is null) {
+			return alloc(size);
+		}
+
+		auto copySize = size;
+		auto pd = getPageDescriptor(ptr);
+
+		if (pd.isSlab()) {
+			auto newSizeClass = getSizeClass(size);
+			auto oldSizeClass = pd.sizeClass;
+			if (newSizeClass == oldSizeClass) {
+				return ptr;
+			}
+
+			if (newSizeClass > oldSizeClass) {
+				copySize = getSizeFromClass(oldSizeClass);
+			}
+		} else {
+			auto esize = pd.extent.size;
+			if (alignUp(size, PageSize) == esize) {
+				return ptr;
+			}
+
+			// TODO: Try to extend/shrink in place.
+			import d.gc.util;
+			copySize = min(size, esize);
+		}
+
+		auto newPtr = alloc(size);
+		if (newPtr is null) {
+			return null;
+		}
+
+		memcpy(newPtr, ptr, copySize);
+		pd.extent.arena.free(pd, ptr);
+
+		return newPtr;
+	}
+
+	/**
+	 * GC facilities
+	 */
+	void addRoots(const void[] range) {
+		auto ptr = cast(void*) roots.ptr;
+
+		// We realloc everytime. It doesn't really matter at this point.
+		roots.ptr = cast(const(void*)[]*)
+			realloc(ptr, (roots.length + 1) * void*[].sizeof);
+
+		// Using .ptr to bypass bound checking.
+		roots.ptr[roots.length] = makeRange(range);
+
+		// Update the range.
+		roots = roots.ptr[0 .. roots.length + 1];
+	}
+
+	void collect() {
+		// TODO: The set need a range interface or some other way to iterrate.
+		// FIXME: Prepare the GC so it has bitfields for all extent classes.
+
+		// Scan the roots !
+		__sdgc_push_registers(scanStack);
+		foreach (range; roots) {
+			scan(range);
+		}
+
+		// TODO: Go on and on until all worklists are empty.
+
+		// TODO: Collect.
+	}
+
+	bool scanStack() {
+		import sdc.intrinsics;
+		auto framePointer = readFramePointer();
+		auto length = stackBottom - framePointer;
+
+		auto range = makeRange(framePointer[0 .. length]);
+		return scan(range);
+	}
+
+	bool scan(const(void*)[] range) {
+		bool newPtr;
+		foreach (ptr; range) {
+			enum PtrMask = ~(AddressSpace - 1);
+			auto iptr = cast(size_t) ptr;
+
+			if (iptr & PtrMask) {
+				// This is not a pointer, move along.
+				// TODO: Replace this with a min-max test.
+				continue;
+			}
+
+			auto pd = maybeGetPageDescriptor(ptr);
+			if (pd.extent is null) {
+				// We have no mappign there.
+				continue;
+			}
+
+			// We have something, mark!
+			newPtr |= true;
+
+			// FIXME: Mark the extent.
+			// FIXME: If the extent may contain pointers,
+			// add the base ptr to the worklist.
+		}
+
+		return newPtr;
+	}
+
+private:
+	auto getPageDescriptor(void* ptr) {
+		auto pd = maybeGetPageDescriptor(ptr);
+		assert(pd.extent !is null);
+		assert(pd.isSlab() || ptr is pd.extent.addr);
+
+		return pd;
+	}
+
+	auto maybeGetPageDescriptor(const void* ptr) {
+		import d.gc.util;
+		auto aptr = alignDown(ptr, PageSize);
+
+		// FIXME: Pass the emap down to the arena instead of
+		// getting it from the arena.
+		auto a = arena.allocator;
+		return a.emap.lookup(aptr);
+	}
+}
+
+private:
+
+extern(C):
+version(OSX) {
+	// For some reason OSX's symbol get a _ prepended.
+	bool _sdgc_push_registers(bool delegate());
+	alias __sdgc_push_registers = _sdgc_push_registers;
+} else {
+	bool __sdgc_push_registers(bool delegate());
+}
+
+/**
+ * This function get a void[] range and chnage it into a
+ * const(void*)[] one, reducing to alignement boundaries.
+ */
+const(void*)[] makeRange(const void[] range) {
+	auto begin = alignUp(range.ptr, PointerSize);
+	auto end = alignDown(range.ptr + range.length, PointerSize);
+
+	auto ibegin = cast(size_t) begin;
+	auto iend = cast(size_t) end;
+	if (ibegin > iend) {
+		return [];
+	}
+
+	auto ptr = cast(void**) begin;
+	auto length = (iend - ibegin) / PointerSize;
+
+	return ptr[0 .. length];
+}
+
+unittest makeRange {
+	static checkRange(const void[] range, size_t start, size_t stop) {
+		auto r = makeRange(range);
+		assert(r.ptr is cast(const void**) start);
+		assert(r.ptr + r.length is cast(const void**) stop);
+	}
+
+	void* ptr;
+	void[] range = ptr[0 .. 5];
+
+	checkRange(ptr[0 .. 0], 0, 0);
+	checkRange(ptr[0 .. 1], 0, 0);
+	checkRange(ptr[0 .. 2], 0, 0);
+	checkRange(ptr[0 .. 3], 0, 0);
+	checkRange(ptr[0 .. 4], 0, 0);
+	checkRange(ptr[0 .. 5], 0, 0);
+	checkRange(ptr[0 .. 6], 0, 0);
+	checkRange(ptr[0 .. 7], 0, 0);
+	checkRange(ptr[0 .. 8], 0, 8);
+
+	checkRange(ptr[1 .. 1], 0, 0);
+	checkRange(ptr[1 .. 2], 0, 0);
+	checkRange(ptr[1 .. 3], 0, 0);
+	checkRange(ptr[1 .. 4], 0, 0);
+	checkRange(ptr[1 .. 5], 0, 0);
+	checkRange(ptr[1 .. 6], 0, 0);
+	checkRange(ptr[1 .. 7], 0, 0);
+	checkRange(ptr[1 .. 8], 8, 8);
+}
