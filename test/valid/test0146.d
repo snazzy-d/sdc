d04905890448a5c0b0ad014d335320e7274c9c43
Amaury Séchet <deadalnix@gmail.com>
2023-12-05 20:05:09 +0000

    Remove obsolete test (superseeded by oop.d)

--- test/valid/test0146.d
+++ test/valid/test0146.d
@@ -1,39 +1,18 @@
 //T compiles:yes
-//T has-passed:yes
 //T retval:0
-// downcast
+//T has-passed:yes
+// Tests pointer casts.
 
-class A {}
+struct S {
+	uint value;
+}
 
-class B : A {}
+int main() {
+	S s;
+	s.value = 123;
 
-final class C : B {}
+	auto ptr = cast(uint*) &s;
+	assert(*ptr == 123);
 
-void main() {
-	Object a = new A();
-	auto aa = cast(A) a;
-	auto ab = cast(B) a;
-	auto ac = cast(C) a;
-
-	assert(aa is a);
-	assert(ab is null);
-	assert(ac is null);
-
-	Object b = new B();
-	auto ba = cast(A) b;
-	auto bb = cast(B) b;
-	auto bc = cast(C) b;
-
-	assert(ba is b);
-	assert(bb is b);
-	assert(bc is null);
-
-	Object c = new C();
-	auto ca = cast(A) c;
-	auto cb = cast(B) c;
-	auto cc = cast(C) c;
-
-	assert(ca is c);
-	assert(cb is c);
-	assert(cc is c);
+	return 0;
 }
