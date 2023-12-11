fb7ed0ddc9ec58bba05f6b1eb57ed3742fe89e3a
Amaury Séchet <deadalnix@gmail.com>
2023-12-11 23:47:57 +0000

    Allocate metadata by pages from blocks.

--- sdlib/d/gc/base.d
+++ sdlib/d/gc/base.d
@@ -16,11 +16,13 @@ private:
 	import d.sync.mutex;
 	Mutex mutex;
 
-	ubyte currentGeneration;
-
-	// The slice of memory we have to allocate from.
-	size_t availableMetadatSlots;
+	// The slice of memory we have to allocate slots from.
 	Slot nextSlot;
+
+	// The slice of memory we allocate metadata from.
+	void* nextMetadataPage;
+	uint availableMetadataPages;
+	ubyte currentGeneration;
 
 	// Linked list of all the blocks.
 	Block* head;
@@ -66,6 +68,13 @@ public:
 		return (cast(Base*) &this).allocSlotImpl();
 	}
 
+	Slot allocMetadataPage() shared {
+		mutex.lock();
+		scope(exit) mutex.unlock();
+
+		return (cast(Base*) &this).allocMetadataPageImpl();
+	}
+
 	void* reserveAddressSpace(size_t size) shared {
 		// Bump the alignement to block size if apropriate.
 		auto alignment = isAligned(size, BlockSize) ? BlockSize : PageSize;
@@ -97,19 +106,53 @@ private:
 	Slot allocSlotImpl() {
 		assert(mutex.isHeld(), "Mutex not held!");
 
-		if (!refillSlots()) {
+		if (hasFreeSlot()) {
+			goto Success;
+		}
+
+		// Eagerly try to refill, because it may allocate a slot
+		// and we want to catch this rather than void a whole page.
+		if (!refillMetadataSpace()) {
 			return Slot(null, 0);
 		}
 
-		assert(hasFreeSlot(), "No Metadata slot available!");
+		// We may have allocated a slot to store block informations,
+		// so we need to check again as we might be good to go.
+		if (hasFreeSlot()) {
+			goto Success;
+		}
 
+		nextSlot = allocMetadataPageImpl();
+		if (nextSlot.address is null) {
+			return Slot(null, 0);
+		}
+
+	Success:
 		scope(success) {
 			auto nextAddress = nextSlot.address + ExtentSize;
-			availableMetadatSlots -= 1;
 			nextSlot = Slot(nextAddress, nextSlot.generation);
 		}
 
 		return nextSlot;
+	}
+
+	Slot allocMetadataPageImpl() {
+		assert(mutex.isHeld(), "Mutex not held!");
+
+		if (!refillMetadataSpace()) {
+			return Slot(null, 0);
+		}
+
+		assert(availableMetadataPages > 0, "No Metadata page available!");
+		assert(isAligned(nextMetadataPage, PageSize),
+		       "Invalid nextMetadataPage alignment!");
+
+		scope(success) {
+			nextMetadataPage += PageSize;
+			availableMetadataPages--;
+		}
+
+		return Slot(nextMetadataPage, currentGeneration);
 	}
 
 	void* reserveAddressSpaceImpl(size_t size, size_t alignment) {
@@ -185,28 +228,28 @@ private:
 	}
 
 	bool hasFreeSlot() {
-		return availableMetadatSlots > 0;
+		return !isAligned(nextSlot.address, PageSize);
 	}
 
-	bool refillSlots() {
+	bool refillMetadataSpace() {
 		assert(mutex.isHeld(), "Mutex not held!");
 
-		if (hasFreeSlot()) {
+		if (availableMetadataPages > 0) {
 			return true;
 		}
 
 		// Allocate exponentially more space for metadata.
 		auto shift = currentGeneration++;
-		auto size = PageSize << shift;
+		auto size = BlockSize << shift;
 
 		import d.gc.memmap;
-		auto ptr = pages_map(null, size, PageSize);
+		auto ptr = pages_map(null, size, BlockSize);
 		if (ptr is null) {
 			return false;
 		}
 
-		availableMetadatSlots = size / ExtentSize;
-		nextSlot = Slot(ptr, currentGeneration);
+		nextMetadataPage = ptr;
+		availableMetadataPages = PagesInBlock << shift;
 
 		// We expect this allocation to always succeed as we just
 		// reserved a ton of address space.
@@ -232,7 +275,7 @@ private:
 		// Because refilling metadata space may allocate blocks
 		// and we are about to use some metadata space, we want
 		// to double check the free list.
-		if (!refillSlots()) {
+		if (!refillMetadataSpace()) {
 			return false;
 		}
 
@@ -290,14 +333,12 @@ unittest base {
 	auto b0 = base.allocBlock();
 	auto b1 = base.allocBlock();
 	assert(b0 + 1 is b1);
-	assert(base.availableMetadatSlots == 31);
 
 	// We get the same block recycled.
 	base.freeBlock(b0);
 	base.freeBlock(b1);
 	assert(base.allocBlock() is b1);
 	assert(base.allocBlock() is b0);
-	assert(base.availableMetadatSlots == 31);
 
 	// Now allocate slots.
 	auto s0 = base.allocSlot();
@@ -305,10 +346,9 @@ unittest base {
 	assert(s0.address is alignUp(b0, ExtentSize));
 	assert(s0.address + ExtentSize is s1.address);
 	assert(s0.generation == s1.generation);
-	assert(base.availableMetadatSlots == 29);
 
 	auto prev = s1;
-	foreach (i; 0 .. 29) {
+	foreach (i; 0 .. 16381) {
 		auto current = base.allocSlot();
 		assert(prev.address + ExtentSize is current.address);
 		assert(prev.generation == current.generation);
@@ -318,5 +358,69 @@ unittest base {
 
 	auto n = base.allocSlot();
 	assert(prev.generation + 1 == n.generation);
-	assert(base.availableMetadatSlots == 63);
+}
+
+unittest count_blocks {
+	shared Base base;
+	scope(exit) base.clear();
+
+	static countBlocks(ref shared Base base) {
+		uint ret = 0;
+
+		auto current = cast(Block*) base.head;
+		while (current !is null) {
+			current = current.next;
+			ret++;
+		}
+
+		return ret;
+	}
+
+	static size_t indexInBlock(void* ptr) {
+		auto v = cast(size_t) ptr;
+		return (v % BlockSize) / ExtentSize;
+	}
+
+	static size_t indexInBlock(Base.Slot s) {
+		return indexInBlock(s.address);
+	}
+
+	// Almost fill in a block of metadata.
+	auto prev = base.allocSlot();
+	assert(indexInBlock(prev) == 1);
+
+	foreach (i; 2 .. 16382) {
+		auto current = base.allocSlot();
+		assert(indexInBlock(current) == i);
+		assert(countBlocks(base) == 1);
+		assert(prev.generation == current.generation);
+
+		prev = current;
+	}
+
+	// Empty the free list.
+	while (base.blockFreeList !is null) {
+		base.allocBlock();
+		assert(countBlocks(base) == 1);
+	}
+
+	auto b = base.allocBlock();
+	assert(indexInBlock(b) == 16382);
+	assert(countBlocks(base) == 1);
+
+	auto current = base.allocSlot();
+	assert(indexInBlock(current) == 16383);
+	assert(countBlocks(base) == 1);
+	assert(prev.generation == current.generation);
+
+	// Empty the free list.
+	while (base.blockFreeList !is null) {
+		base.allocBlock();
+		assert(countBlocks(base) == 1);
+	}
+
+	current = base.allocSlot();
+	assert(indexInBlock(current) == 1);
+	assert(countBlocks(base) == 2);
+	assert(prev.generation + 1 == current.generation);
 }
