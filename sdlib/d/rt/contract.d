635817627c99c925c8a94f943b3692a415bff631
Amaury Séchet <deadalnix@gmail.com>
2024-01-08 16:15:17 +0000

    Fix contract fairlure error messages.

--- sdlib/d/rt/contract.d
+++ sdlib/d/rt/contract.d
@@ -9,15 +9,16 @@ extern(C):
 void __sd_assert_fail(string file, int line) {
 	import core.stdc.stdlib, core.stdc.unistd, core.stdc.stdio;
 	auto len = snprintf(_dbgPrintBuffer.ptr, DbgPrintBufferSize,
-	                    "assert fail: %s:%d\n", file.ptr, line);
+	                    "%.*s:%d: assert fail.\n", file.length, file.ptr, line);
 	write(STDERR_FILENO, _dbgPrintBuffer.ptr, len);
 	exit(1);
 }
 
 void __sd_assert_fail_msg(string msg, string file, int line) {
 	import core.stdc.stdlib, core.stdc.unistd, core.stdc.stdio;
-	auto len = snprintf(_dbgPrintBuffer.ptr, DbgPrintBufferSize, "%s: %s:%d\n",
-	                    msg.ptr, file.ptr, line);
+	auto len =
+		snprintf(_dbgPrintBuffer.ptr, DbgPrintBufferSize, "%.*s:%d: %.*s\n",
+		         file.length, file.ptr, line, msg.length, msg.ptr);
 	write(STDERR_FILENO, _dbgPrintBuffer.ptr, len);
 	exit(1);
 }
