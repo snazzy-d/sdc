cad5204fc372ffbb1eed2227c6692275dfbf5a7f
Amaury Séchet <deadalnix@gmail.com>
2024-01-08 16:31:23 +0000

    Refactor assertion failure to extract a functiont o print the error.

--- sdlib/d/rt/contract.d
+++ sdlib/d/rt/contract.d
@@ -6,19 +6,24 @@ static char[DbgPrintBufferSize] _dbgPrintBuffer;
 
 extern(C):
 
-void __sd_assert_fail(string file, int line) {
-	import core.stdc.stdlib, core.stdc.unistd, core.stdc.stdio;
-	auto len = snprintf(_dbgPrintBuffer.ptr, DbgPrintBufferSize,
-	                    "%.*s:%d: assert fail.\n", file.length, file.ptr, line);
+void __sd_print_debug_message(string file, int line, string message) {
+	import core.stdc.unistd, core.stdc.stdio;
+	auto len =
+		snprintf(_dbgPrintBuffer.ptr, DbgPrintBufferSize, "%.*s:%d: %.*s\n",
+		         file.length, file.ptr, line, message.length, message.ptr);
 	write(STDERR_FILENO, _dbgPrintBuffer.ptr, len);
+}
+
+void __sd_assert_fail(string file, int line) {
+	__sd_print_debug_message(file, line, "assert failure.");
+
+	import core.stdc.stdlib;
 	exit(1);
 }
 
 void __sd_assert_fail_msg(string msg, string file, int line) {
-	import core.stdc.stdlib, core.stdc.unistd, core.stdc.stdio;
-	auto len =
-		snprintf(_dbgPrintBuffer.ptr, DbgPrintBufferSize, "%.*s:%d: %.*s\n",
-		         file.length, file.ptr, line, msg.length, msg.ptr);
-	write(STDERR_FILENO, _dbgPrintBuffer.ptr, len);
+	__sd_print_debug_message(file, line, msg);
+
+	import core.stdc.stdlib;
 	exit(1);
 }
