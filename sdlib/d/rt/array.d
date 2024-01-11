6ef20e00ecf6f5c71be61166fc1330bd01d301ce
Amaury Séchet <deadalnix@gmail.com>
2024-01-11 00:58:43 +0000

    Use __sd_assert_fail_msg in __sd_array_outofbounds .

--- sdlib/d/rt/array.d
+++ sdlib/d/rt/array.d
@@ -3,7 +3,6 @@ module d.rt.array;
 extern(C):
 
 void __sd_array_outofbounds(string file, int line) {
-	import core.stdc.stdlib, core.stdc.stdio;
-	printf("bound check fail: %s:%d\n", file.ptr, line);
-	exit(1);
+	import d.rt.contract;
+	__sd_assert_fail_msg("bound check fail.", file, line);
 }
