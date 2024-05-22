ada39400157a1ddb58e57b857b9d298ce9fefd37
Steven Schveighoffer <schveiguy@gmail.com>
2024-05-22 17:02:19 -0400

    Use extern(C) call for druntime for thread scanning. (#369)
    * Use extern(C) call for druntime for thread scanning.
    
    * Remove unnecessary struct complication in dmd calls.
    
    * Reorder context parameter

--- sdlib/dmd/thread.d
+++ sdlib/dmd/thread.d
@@ -6,18 +6,24 @@ extern(C):
 void thread_suspendAll();
 void thread_resumeAll();
 
-alias ScanAllThreadsFn = void delegate(void*, void*);
-void thread_scanAll(ScanAllThreadsFn scan);
+// note, we must interface with DMD using extern(C) calls, so we
+// cannot call thread_scanAll directly. This requires a hook compiled
+// on the druntime side that forwards to thread_scanAll, and we cannot
+// use delegates (as those are not the same ABI)
+// the context pointer is just a pass-through for the druntime side, so we
+// type it based on what we are passing.
+alias ScanFn = bool delegate(const(void*)[] range);
+void __sd_scanAllThreadsFn(ScanFn* context, void* start, void* end) {
+	import d.gc.range;
+	(*context)(makeRange(start, end));
+}
+
+// defined in druntime. The context pointer gets passed to the scan routine
+void thread_scanAll_C(ScanFn* context, typeof(&__sd_scanAllThreadsFn) scan);
 
 // sdrt API.
-alias ScanFn = bool delegate(const(void*)[] range);
 void __sd_thread_scan(ScanFn scan) {
-	auto ts = ThreadScanner(scan);
-
-	// FIXME: For some reason, this doesn't work
-	// if the literal is passed directly.
-	auto dg = ts.scanRange;
-	thread_scanAll(dg);
+	thread_scanAll_C(&scan, &__sd_scanAllThreadsFn);
 }
 
 void __sd_thread_stop_the_world() {
@@ -34,18 +40,3 @@ void __sd_thread_create() {
 }
 
 void __sd_thread_destroy() {}
-
-private:
-
-struct ThreadScanner {
-	ScanFn scan;
-
-	this(ScanFn scan) {
-		this.scan = scan;
-	}
-
-	extern(C) void scanRange(void* start, void* stop) {
-		import d.gc.range;
-		scan(makeRange(start, stop));
-	}
-}
