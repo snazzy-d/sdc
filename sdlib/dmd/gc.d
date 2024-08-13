f0539dc639330211ea14be17b62549673d309c82
Steven Schveighoffer <schveiguy@gmail.com>
2024-08-13 12:22:02 -0400

    Finish hooks for array management from druntime

--- sdlib/dmd/gc.d
+++ sdlib/dmd/gc.d
@@ -3,14 +3,15 @@ module dmd.gc;
 import d.gc.tcache;
 import d.gc.spec;
 import d.gc.slab;
+import d.gc.emap;
 
 extern(C):
 
 // Some of the bits from druntime.
 enum BlkAttr : uint {
-    FINALIZE = 0b0000_0001,
-    NO_SCAN = 0b0000_0010,
-    APPENDABLE = 0b0000_1000
+	FINALIZE = 0b0000_0001,
+	NO_SCAN = 0b0000_0010,
+	APPENDABLE = 0b0000_1000
 }
 
 // ptr: the pointer to query
@@ -18,54 +19,87 @@ enum BlkAttr : uint {
 // size: => the full usable size of the block
 // pd: => the page descriptor (used internally later)
 // flags: => what the flags of the block are
-// 
-bool __sd_gc_fetch_alloc_info(void* ptr, void** base, size_t* size, PageDescriptor* pd, BlkAttr* flags) {
-    *pd = threadCache.maybeGetPageDescriptor(ptr);
-    auto e = pd.extent;
-    *flags = BlkAttr.init;
-    if (e) {
-        if (!pd.containsPointers) {
-            *flags |= BlkAttr.NO_SCAN;
-        }
+//
+bool __sd_gc_fetch_alloc_info(void* ptr, void** base, size_t* size,
+                              PageDescriptor* pd, BlkAttr* flags) {
+	*pd = threadCache.maybeGetPageDescriptor(ptr);
+	auto e = pd.extent;
+	*flags = cast(BlkAttr) 0;
+	if (e) {
+		if (!pd.containsPointers) {
+			*flags |= BlkAttr.NO_SCAN;
+		}
 
-        if (pd.isSlab()) {
-            auto si = SlabAllocInfo(pd, ptr);
-            *base = cast(void*) si.address;
+		if (pd.isSlab()) {
+			auto si = SlabAllocInfo(*pd, ptr);
+			*base = cast(void*) si.address;
 
-            if(si.hasMetadata) {
-                *flags |= BlkAttr.APPENDABLE;
-                if(si.hasFinalizer) {
-                    *flags |= BlkAttr.FINALIZE;
-                }
-            }
-            *size = si.capacity;
-        } else {
-            // large blocks are always appendable.
-            *flags |= BlkAttr.APPENDABLE;
+			if (si.hasMetadata) {
+				*flags |= BlkAttr.APPENDABLE;
+				if (si.finalizer) {
+					*flags |= BlkAttr.FINALIZE;
+				}
+			}
 
-            if(e.finalizer) {
-                *flags |= BlkAttr.FINALIZE;
-            }
+			*size = si.slotCapacity;
+		} else {
+			// large blocks are always appendable.
+			*flags |= BlkAttr.APPENDABLE;
 
-            auto e = pd.extent;
-            *base = e.address;
+			if (e.finalizer) {
+				*flags |= BlkAttr.FINALIZE;
+			}
 
-            *size = e.size;
-        }
-        return true;
-    }
-    return false; 
+			auto e = pd.extent;
+			*base = e.address;
+
+			*size = e.size;
+		}
+
+		return true;
+	}
+
+	return false;
 }
 
-// only for large blocks. For slots, the other side will figure it out.
-bool __sd_gc_set_array_used(void* ptr, PageDescriptor pd, size_t newUsed, size_t existingUsed) {
-    auto e = pd.extent;
-    if (!e) {
-        return false;
-    }
+size_t __sd_gc_get_array_used(void* ptr, PageDescriptor pd) {
+	auto e = pd.extent;
+	if (!e) {
+		return 0;
+	}
 
-    if(pd.isSlab()) {
+	if (pd.isSlab()) {
+		auto si = SlabAllocInfo(pd, ptr);
+		return si.usedCapacity;
+	} else {
+		return e.usedCapacity;
+	}
+}
 
-    } else {
-    }
+// TODO: this should only do large blocks, and let the druntime side do slabs.
+bool __sd_gc_set_array_used(void* ptr, PageDescriptor pd, size_t newUsed,
+                            size_t existingUsed) {
+	auto e = pd.extent;
+	if (!e) {
+		return false;
+	}
+
+	if (pd.isSlab()) {
+		auto si = SlabAllocInfo(pd, ptr);
+		if (existingUsed < size_t.max && existingUsed != si.usedCapacity) {
+			// The existing capacity doesn't match.
+			return false;
+		}
+
+		return si.setUsedCapacity(newUsed);
+	} else {
+		if (existingUsed < size_t.max && existingUsed != e.usedCapacity) {
+			// The existing capacity doesn't match.
+			return false;
+		}
+
+		e.setUsedCapacity(newUsed);
+	}
+
+	return true;
 }
