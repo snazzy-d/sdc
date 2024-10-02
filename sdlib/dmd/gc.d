a3a2555cafaba00df0ec97fd5ca9cc95a27bd5e0
Steven Schveighoffer <schveiguy@gmail.com>
2024-10-02 00:33:29 -0400

    Add functions to do both extending and shrinking used capacity.

--- sdlib/dmd/gc.d
+++ sdlib/dmd/gc.d
@@ -67,23 +67,12 @@ bool __sd_gc_fetch_alloc_info(void* ptr, void** base, size_t* size,
 	return true;
 }
 
-size_t __sd_gc_get_array_used(void* ptr, PageDescriptor pd) {
-	auto e = pd.extent;
-	if (!e) {
-		return 0;
-	}
-
-	if (pd.isSlab()) {
-		auto si = SlabAllocInfo(pd, ptr);
-		return si.usedCapacity;
-	} else {
-		return e.usedCapacity;
-	}
+void[] __sd_gc_get_allocation_slice(const void* ptr) {
+	return threadCache.getAllocationSlice(ptr);
 }
 
-// TODO: this should only do large blocks, and let the druntime side do slabs.
-bool __sd_gc_set_array_used(void* ptr, PageDescriptor pd, size_t newUsed,
-                            size_t existingUsed) {
+bool __sd_gc_shrink_array_used(void* ptr, size_t newUsed, size_t existingUsed) {
+	auto pd = threadCache.maybeGetPageDescriptor(ptr);
 	auto e = pd.extent;
 	if (!e) {
 		return false;
@@ -91,21 +80,46 @@ bool __sd_gc_set_array_used(void* ptr, PageDescriptor pd, size_t newUsed,
 
 	if (pd.isSlab()) {
 		auto si = SlabAllocInfo(pd, ptr);
-		if (existingUsed < size_t.max && existingUsed != si.usedCapacity) {
-			// The existing capacity doesn't match.
+		auto offset = ptr - si.address;
+		if(existingUsed != 0 && si.usedCapacity != existingUsed + offset) {
 			return false;
 		}
-
-		return si.setUsedCapacity(newUsed);
-	} else {
-		if (existingUsed < size_t.max && existingUsed != e.usedCapacity) {
-			// The existing capacity doesn't match.
-			return false;
-		}
-
-		e.setUsedCapacity(newUsed);
+		assert(newUsed + offset <= si.usedCapacity);
+		return si.setUsedCapacity(newUsed + offset);
 	}
 
+	// Large allocation.
+	auto offset = ptr - e.address;
+	if(existingUsed != 0 && e.usedCapacity != existingUsed + offset) {
+		return false;
+	}
+	assert(newUsed + offset <= e.usedCapacity);
+	e.setUsedCapacity(newUsed + offset);
+	return true;
+}
+
+bool __sd_gc_extend_array_used(void* ptr, size_t newUsed, size_t existingUsed) {
+	// extend array to new used + 1, but don't set the size. Then set the size
+	// if it succeeds.
+	assert(newUsed > existingUsed);
+	if(!threadCache.reserve(ptr[0 .. existingUsed], newUsed - existingUsed + 1))
+	{
+		return false;
+	}
+
+	// We shouldn't have to do this, extend should take a capacity
+	// parameter like malloc.
+	auto pd = threadCache.maybeGetPageDescriptor(ptr);
+	auto e = pd.extent;
+	if (pd.isSlab()) {
+		auto si = SlabAllocInfo(pd, ptr);
+		auto offset = ptr - si.address;
+		return si.setUsedCapacity(newUsed + offset);
+	}
+
+	// Large allocation.
+	auto offset = ptr - e.address;
+	e.setUsedCapacity(newUsed + offset);
 	return true;
 }
 
