6b70ec05e5a6a5f55d22301d4d5f56d67915adb8
Amaury Séchet <deadalnix@gmail.com>
2024-10-12 21:32:35 +0000

    Declare C malloc/free in stdlib.d

--- sdlib/core/stdc/stdlib.d
+++ sdlib/core/stdc/stdlib.d
@@ -1,8 +1,14 @@
 module core.stdc.stdlib;
 
-extern (C):
+extern(C):
+
 // @trusted: // Types only.
 // nothrow:
 // @nogc:
+
+void* malloc(size_t size);
+void free(void* ptr);
+void* calloc(size_t nmemb, size_t size);
+void* realloc(void* ptr, size_t size);
 
 void exit(int code);
