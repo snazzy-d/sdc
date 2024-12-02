c83a32b1b863da1c44533397202f39b4d517b543
Steven Schveighoffer <schveiguy@gmail.com>
2024-12-02 10:13:27 -0500

    [GC] Remove threadpool from scanner use for now.

--- sdlib/d/gc/scanner.d
+++ sdlib/d/gc/scanner.d
@@ -55,22 +55,35 @@ public:
 		auto threadCount = activeThreads - 1;
 		auto threadsPtr =
 			cast(pthread_t*) alloca(pthread_t.sizeof * threadCount);
-
 		auto threads = threadsPtr[0 .. threadCount];
-		import d.gc.threadpool;
-		shared ThreadPool threadPool;
-		threadPool.startThreads(threads);
+
+		static void* markThreadEntry(void* ctx) {
+			import d.gc.tcache;
+			threadCache.activateGC(false);
+
+			(cast(shared(Scanner*)) ctx).runMark();
+			return null;
+		}
+
+		// First thing, start the worker threads, so they can do work ASAP.
+		foreach (ref tid; threads) {
+			pthread_create(&tid, null, markThreadEntry, cast(void*) &this);
+		}
 
 		// Scan the roots.
 		__sd_gc_global_scan(addToWorkList);
 
-		threadPool.dispatch(runMark, activeThreads, true);
+		// Now send this thread marking!
+		runMark();
 
-		threadPool.joinAll();
-
-		// We now are done, we can free the worklist.
+		// We now done, we can free the worklist.
 		import d.gc.tcache;
 		threadCache.free(cast(void*) worklist.ptr);
+
+		foreach (tid; threads) {
+			void* ret;
+			pthread_join(tid, &ret);
+		}
 	}
 
 	void addToWorkList(WorkItem[] items) shared {
@@ -105,11 +118,7 @@ public:
 	}
 
 private:
-	void runMark(uint idx) shared {
-		import d.gc.tcache;
-		// Note: in the main thrad, the GC will be reactivated later,
-		// so it is always safe to do this here.
-		threadCache.activateGC(false);
+	void runMark() shared {
 		auto worker = Worker(&this);
 
 		/**
