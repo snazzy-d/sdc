039b446369ffe34ca811b67984f309177adcfb8f
Amaury Séchet <deadalnix@gmail.com>
2025-06-30 15:52:46 +0000

    Add a test for a parsing bug in DMD.

--- test/unit/aliases.d
+++ test/unit/aliases.d
@@ -1,7 +1,20 @@
 alias T = uint;
 alias A = T[7];
 
-unittest aliases {
+unittest types {
 	assert(T.sizeof == 4);
 	assert(A.sizeof == 28);
+}
+
+enum E {
+	A,
+	B,
+}
+
+alias EA = E.A;
+alias EB = E.B;
+alias ESame = EA == EB;
+
+unittest expressions {
+	assert(ESame == false);
 }
